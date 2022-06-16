a8f927835e8eaceea4684d81d57c87b0041ee7b2
andyv09 <andraz.vrecko@gmail.com>
2022-06-16 11:02:23 +0200

    added support for MetaMask accounts

--- src/utils/veramo_utils.ts
+++ src/utils/veramo_utils.ts
@@ -1,11 +1,12 @@
-import { agent, fillDids } from "../veramo/setup";
+import { agent } from "../veramo/setup";
 import {
   IIdentifier,
   MinimalImportableKey,
   VerifiableCredential,
   VerifiablePresentation,
 } from "@veramo/core";
-import { checkForDelegate, getCurrentAccount } from "./snap_utils";
+import { getCurrentAccount } from "./snap_utils";
+import { getVCAccount } from "./state_utils";
 declare let wallet: any;
 
 /**
@@ -61,62 +62,61 @@ export async function create_vp(
   challenge?: string,
   domain?: string
 ): Promise<VerifiablePresentation | null> {
-  //let identifier = await get_id();
-  let identifier = "did:ethr:0x4:" + (await getCurrentAccount());
+  let identifier = await importMetaMaskAccount();
 
-  const res = await checkForDelegate();
-  if (res) {
-    const vc = await agent.getVC({ id: vc_id });
-    if (vc.vc != null) {
-      const result = await wallet.request({
-        method: "snap_confirm",
-        params: [
-          {
-            prompt: `Alert`,
-            description: "Do you wish to create a VP from the following VC?",
-            textAreaContent: JSON.stringify(vc.vc.credentialSubject),
-          },
-        ],
+  const vc = await agent.getVC({ id: vc_id });
+  if (vc.vc != null) {
+    const result = await wallet.request({
+      method: "snap_confirm",
+      params: [
+        {
+          prompt: `Alert`,
+          description: "Do you wish to create a VP from the following VC?",
+          textAreaContent: JSON.stringify(vc.vc.credentialSubject),
+        },
+      ],
+    });
+    console.log("RESULT", result);
+    console.log("VC", vc);
+    if (result) {
+      if (challenge) console.log("Challenge:", challenge);
+      if (domain) console.log("Domain:", domain);
+      const vp = await agent.createVerifiablePresentation({
+        presentation: {
+          holder: identifier,
+          type: ["VerifiablePresentation", "Custom"],
+          verifiableCredential: [vc.vc],
+        },
+        challenge: challenge,
+        domain: domain,
+        proofFormat: "EthereumEip712Signature2021",
+        save: false,
       });
-      console.log("RESULT", result);
-      if (result) {
-        if (challenge) console.log("Challenge:", challenge);
-        if (domain) console.log("Domain:", domain);
-        const vp = await agent.createVerifiablePresentation({
-          presentation: {
-            holder: identifier,
-            verifier: [],
-            verifiableCredential: [vc.vc],
-          },
-          challenge: challenge,
-          domain: domain,
-          proofFormat: "EthereumEip712Signature2021",
-          save: false,
-        });
-        console.log("....................VP..................");
-        console.log(vp);
-        return vp as VerifiablePresentation;
-      } else {
-        return null;
-      }
+      console.log("....................VP..................");
+      console.log(vp);
+      return vp as VerifiablePresentation;
     } else {
-      console.log("No VC found...");
       return null;
     }
   } else {
+    console.log("No VC found...");
     return null;
   }
 }
 
-export async function create_vc() {
-  // await fillDids();
-
-  // console.log(identifier);
-
-  // let identifier = "did:ethr:0x4:" + (await getCurrentAccount());
-
+export const importMetaMaskAccount = async (): Promise<string> => {
   const account = await getCurrentAccount();
   let did = "did:ethr:0x4:" + account;
+
+  const identifiers = agent.didManagerFind();
+  let exists = false;
+  (await identifiers).map((id) => {
+    if (id.did == did) exists = true;
+  });
+  if (exists) {
+    console.log("DID already exists");
+    return did;
+  }
   const controllerKeyId = `metamask-${account}`;
   await agent.didManagerImport({
     did,
@@ -136,6 +136,38 @@ export async function create_vc() {
       } as MinimalImportableKey,
     ],
   });
+  return did;
+};
+
+export async function create_vc() {
+  const acc = await getVCAccount();
+  console.log("Account state", acc);
+
+  const id = await agent.didManagerCreate({
+    provider: "snap",
+  });
+
+  //const account = await getCurrentAccount();
+  // let did = "did:ethr:0x4:" + account;
+  // // const controllerKeyId = `metamask-${account}`;
+  // // await agent.didManagerImport({
+  // //   did,
+  // //   provider: "metamask",
+  // //   controllerKeyId,
+  // //   keys: [
+  // //     {
+  // //       kid: controllerKeyId,
+  // //       type: "Secp256k1",
+  // //       kms: "web3",
+  // //       privateKeyHex: "",
+  // //       meta: {
+  // //         provider: "metamask",
+  // //         account: account.toLocaleLowerCase(),
+  // //         algorithms: ["eth_signMessage", "eth_signTypedData"],
+  // //       },
+  // //     } as MinimalImportableKey,
+  // //   ],
+  // // });
 
   const identifiers = await agent.didManagerFind();
 
@@ -145,12 +177,12 @@ export async function create_vc() {
       console.log("..................");
     });
   }
-  console.log("Resolving did...");
+  // console.log("Resolving did...");
   //const result = await agent.resolveDid({ didUrl: did });
 
   const result = await agent.createVerifiableCredential({
     credential: {
-      issuer: { id: did },
+      issuer: { id: id.did },
       "@context": [
         "https://www.w3.org/2018/credentials/v1",
         "https://example.com/1/2/3",
@@ -164,6 +196,6 @@ export async function create_vc() {
     },
     proofFormat: "EthereumEip712Signature2021",
   });
-
+  console.log("RESULT", result);
   return result;
 }
--- src/veramo/setup.ts
+++ src/veramo/setup.ts
@@ -5,7 +5,6 @@ import {
   IResolver,
   IDataStore,
   IKeyManager,
-  MinimalImportableKey,
 } from "@veramo/core";
 
 import { AbstractIdentifierProvider, DIDManager } from "@veramo/did-manager";
@@ -37,7 +36,6 @@ import {
   ICredentialIssuer,
   W3cMessageHandler,
 } from "@veramo/credential-w3c";
-import { getCurrentAccount } from "../utils/snap_utils";
 
 const INFURA_PROJECT_ID = "6e751a2e5ff741e5a01eab15e4e4a88b";
 
@@ -47,8 +45,15 @@ const web3Providers: Record<string, Web3Provider> = {};
 const didProviders: Record<string, AbstractIdentifierProvider> = {};
 
 web3Providers["metamask"] = new Web3Provider(wallet);
+
 didProviders["metamask"] = new EthrDIDProvider({
   defaultKms: "web3",
+  network: "rinkeby",
+  web3Provider: new Web3Provider(wallet),
+  rpcUrl: "https://rinkeby.infura.io/v3/" + INFURA_PROJECT_ID,
+});
+didProviders["snap"] = new EthrDIDProvider({
+  defaultKms: "snap",
   network: "rinkeby",
   web3Provider: new Web3Provider(wallet),
   rpcUrl: "https://rinkeby.infura.io/v3/" + INFURA_PROJECT_ID,
@@ -68,37 +73,24 @@ export const agent = createAgent<
       store: new SnapKeyStore(),
       kms: {
         web3: new Web3KeyManagementSystem(web3Providers),
-        //local: new KeyManagementSystem(new SnapPrivateKeyStore()),
+        snap: new KeyManagementSystem(new SnapPrivateKeyStore()),
       },
     }),
     new DIDManager({
       store: new SnapDIDStore(),
       defaultProvider: "metamask",
-      // providers: {
-      //   "did:ethr:rinkeby": new EthrDIDProvider({
-      //     defaultKms: "web3",
-      //     network: "rinkeby",
-      //     web3Provider: new Web3Provider(wallet),
-      //     //rpcUrl: "https://rinkeby.infura.io/v3/" + INFURA_PROJECT_ID,
-      //   }),
-      // },
       providers: didProviders,
     }),
     new VCManager({ store: new SnapVCStore() }),
     new CredentialIssuer(),
     new CredentialIssuerEIP712(),
-    // new MessageHandler({
-    //   messageHandlers: [
-    //     new JwtMessageHandler(),
-    //     new W3cMessageHandler(),
-    //     new SdrMessageHandler(),
-    //   ],
-    // }),
-    // new DIDResolverPlugin({
-    //   resolver: new Resolver({
-    //     ethr: ethrDidResolver({ infuraProjectId: INFURA_PROJECT_ID }).ethr,
-    //   }),
-    // }),
+    new MessageHandler({
+      messageHandlers: [
+        new JwtMessageHandler(),
+        new W3cMessageHandler(),
+        new SdrMessageHandler(),
+      ],
+    }),
     new DIDResolverPlugin({
       resolver: new Resolver({
         ...ethrDidResolver({ infuraProjectId: INFURA_PROJECT_ID }),
@@ -106,27 +98,3 @@ export const agent = createAgent<
     }),
   ],
 });
-
-export const fillDids = async () => {
-  const account = await getCurrentAccount();
-  let did = "did:ethr:0x4:" + account;
-  const controllerKeyId = `metamask-${account}`;
-  await agent.didManagerImport({
-    did,
-    provider: "metamask",
-    controllerKeyId,
-    keys: [
-      {
-        kid: controllerKeyId,
-        type: "Secp256k1",
-        kms: "web3",
-        privateKeyHex: "",
-        meta: {
-          provider: "metamask",
-          account: account.toLocaleLowerCase(),
-          algorithms: ["eth_signMessage", "eth_signTypedData"],
-        },
-      } as MinimalImportableKey,
-    ],
-  });
-};
