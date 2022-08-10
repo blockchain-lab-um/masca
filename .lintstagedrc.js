ad55d536010c318e6e31a5ae354f666fc60cd81f
martines3000 <domajnko.martin@gmail.com>
2022-08-10 12:54:36 +0200

    fix: fixes typo in .lintstagedrc.js

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,5 +1,5 @@
 module.exports = {
-  "*.{js, ts}": ["eslint --fix"],
+  "*.{js,ts}": ["eslint --fix"],
   "*.{md,json,yml,yaml}": ["prettier --write"],
   "*.{ts}": () => "tsc -p tsconfig.json --noEmit --incremental false",
 };
