7d8d1dfa1c6c75d687f32f1ec5a315243f0facb8
martines3000 <domajnko.martin@gmail.com>
2022-08-18 10:46:22 +0200

    feat: basic test config with c8 coverage

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,6 +4,7 @@ module.exports = {
     "plugin:@typescript-eslint/recommended",
     "plugin:@typescript-eslint/recommended-requiring-type-checking",
     "plugin:prettier/recommended",
+    "plugin:mocha/recommended",
     "prettier",
   ],
   parser: "@typescript-eslint/parser",
---
+++ .mocharc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  color: true,
+  extension: "ts",
+  parallel: false,
+  recursive: true,
+  require: "ts-node/register",
+  reporter: "spec",
+  spec: "tests/*.spec.ts",
+};
