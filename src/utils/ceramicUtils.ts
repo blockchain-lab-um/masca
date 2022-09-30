3a1978620d7c516560da0f04d3a397846e05a50d
Andraz <69682837+andyv09@users.noreply.github.com>
2022-09-30 12:23:09 +0200

    fix: ceramic (#21)
    * chore: update version
    
    * chore: update jose
    
    * fix: updated aliases object

--- src/utils/ceramicUtils.ts
+++ src/utils/ceramicUtils.ts
@@ -1,20 +1,32 @@
 import { CeramicClient } from '@ceramicnetwork/http-client';
 import { EthereumAuthProvider } from '@ceramicnetwork/blockchain-utils-linking';
-import { DIDSession } from '@glazed/did-session';
+import { DIDSession } from 'did-session';
 import { getCurrentAccount } from './snapUtils';
 import { DID } from 'dids';
 import { SnapProvider } from '@metamask/snap-types';
 
 const ceramicDID = { did: undefined } as { did: DID | undefined };
 
+// export const aliases = {
+//   definitions: {
+//     StoredCredentials:
+//       'kjzl6cwe1jw148ulgnabbg1vfx1slceuknt57y3gevnf35wgf4e6b15ypqj9ozv',
+//   },
+//   schemas: {
+//     StoredCredentials:
+//       'ceramic://k3y52l7qbv1frxk5wjk8vyexuuev6og9eb7bgrh5xtlepuumjtz7fjf1kuzgmpx4w',
+//   },
+//   tiles: {},
+// };
+
 export const aliases = {
   definitions: {
     StoredCredentials:
-      'kjzl6cwe1jw148ulgnabbg1vfx1slceuknt57y3gevnf35wgf4e6b15ypqj9ozv',
+      'kjzl6cwe1jw1475uoed3zn1yq28pnh6pqqq611y21qwhweln9p8er7g09crnwqa',
   },
   schemas: {
     StoredCredentials:
-      'ceramic://k3y52l7qbv1frxk5wjk8vyexuuev6og9eb7bgrh5xtlepuumjtz7fjf1kuzgmpx4w',
+      'ceramic://k3y52l7qbv1fryllp4tpkqpkbg3ndpni8i4czh2gl8hxjozo5uxduknnd9ebma6m8',
   },
   tiles: {},
 };
@@ -27,16 +39,18 @@ export async function authenticateWithEthereum(
   if (!account) throw Error('User denied error');
   const authProvider = new EthereumAuthProvider(wallet, account);
 
-  const session = new DIDSession({ authProvider });
+  //const session = new DIDSession({});
   typeof window;
 
   // eslint-disable-next-line @typescript-eslint/no-explicit-any, @typescript-eslint/no-unsafe-assignment
   window.location = {} as any;
   window.location.hostname = 'ssi-snap';
 
-  const did = await session.authorize({ domain: 'ssi-snap' });
-  ceramicDID.did = did;
-  return did;
+  const session = await DIDSession.authorize(authProvider, {
+    resources: [`ceramic://*`],
+  });
+  ceramicDID.did = session.did;
+  return session.did;
 }
 
 export async function getCeramic(wallet: SnapProvider): Promise<CeramicClient> {
