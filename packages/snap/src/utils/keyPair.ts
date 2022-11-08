528ca9a8dec88b311ecdbf1efe8c798e8f194751
andyv09 <andraz.vrecko@gmail.com>
2022-11-08 16:12:46 +0100

    fix: add coin_type parameter

--- packages/snap/src/utils/keyPair.ts
+++ packages/snap/src/utils/keyPair.ts
@@ -31,12 +31,15 @@ import { _hexToUnit8Array } from './snapUtils';
 //   };
 // }
 
-export async function getAddressKeyDeriver(wallet: SnapProvider) {
+export async function getAddressKeyDeriver(
+  wallet: SnapProvider,
+  coin_type = 60
+) {
   console.log('before');
   const bip44Node = (await wallet.request({
     method: 'snap_getBip44Entropy',
     params: {
-      coinType: 60,
+      coinType: coin_type,
     },
   })) as BIP44CoinTypeNode;
   return bip44Node;
