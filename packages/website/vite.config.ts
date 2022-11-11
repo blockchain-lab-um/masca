299ecba05220f3dc337d08bc73532ad42dfdd1a6
Martin Domajnko <35891136+martines3000@users.noreply.github.com>
2022-11-11 16:15:04 +0100

    chore: create website project (#48)

---
+++ packages/website/vite.config.ts
@@ -0,0 +1,22 @@
+import { fileURLToPath, URL } from "node:url";
+
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  optimizeDeps: {
+    include: ["@blockchain-lab-um/ssi-snap-connector"],
+  },
+  build: {
+    commonjsOptions: {
+      include: ["@blockchain-lab-um/ssi-snap-connector", /node_modules/],
+    },
+  },
+  plugins: [vue()],
+  resolve: {
+    alias: {
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
+});
