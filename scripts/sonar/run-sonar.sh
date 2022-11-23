e83bfc056b840857df1ce71a5df324599a372298
martines3000 <domajnko.martin@gmail.com>
2022-11-23 13:13:59 +0100

    fix: sonar final version

--- scripts/sonar/run-sonar.sh
+++ scripts/sonar/run-sonar.sh
@@ -72,7 +72,6 @@ info "Analyze projects..."
 # Loop over packages subdirectories.
 for package in packages/*; do
   dir="/github/workspace/${package%*/}" # remove the trailing "/"
-  ls -l
   if [[ -f "$package/sonar-project.properties" ]]; then
     echo "Analyzing $package..."
     docker run -v `pwd`:/github/workspace/ --workdir /github/workspace --network $network --env INPUT_PROJECTBASEDIR=$dir --env SONAR_TOKEN=$SONAR_TOKEN --env SONAR_HOST_URL=$SONAR_HOST_URL sonarsource/sonarqube-scan-action
