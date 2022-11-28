4b345dd002e439fd60d57605fa145778947fbeea
martines3000 <domajnko.martin@gmail.com>
2022-11-28 19:19:48 +0100

    fix: update queryVCs

--- packages/snap/src/rpc/vc/queryVCs.ts
+++ packages/snap/src/rpc/vc/queryVCs.ts
@@ -5,30 +5,32 @@ import { snapConfirm } from '../../utils/snapUtils';
 import { ApiParams } from '../../interfaces';
 import { AvailableVCStores } from 'src/constants';
 
+type QueryFilter = { type: string; filter: unknown };
+type QueryOptions = {
+  store?: AvailableVCStores | [AvailableVCStores];
+  returnStore?: boolean;
+};
+
 export async function queryVCs(
   params: ApiParams,
-  filter?: { type: string; filter: any },
-  options?: {
-    store?: AvailableVCStores | [AvailableVCStores];
-    returnStore?: boolean;
-  }
+  filter?: QueryFilter,
+  options?: QueryOptions
 ): Promise<VerifiableCredential[]> {
-  let store = options?.store;
-  let retStore = options?.returnStore;
-  if (typeof store === 'undefined') {
-    store = ['snap'];
-  }
+  let { store = ['snap'] } = options || {};
+  // TODO: Remove this when we start using the returnStore option
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const { returnStore = false } = options || {};
+
   if (typeof store === 'string') {
     store = [store];
   }
-  if (typeof retStore === 'undefined') {
-    retStore = false;
-  }
+
   if (typeof filter === 'undefined') {
     filter = { type: 'none', filter: {} };
   }
-  const { state, wallet, account } = params;
-  const vcs = await veramoListVCs(wallet, store, filter?.filter as VCQuery);
+
+  const { state, wallet } = params;
+  const vcs = await veramoListVCs(wallet, store, filter.filter as VCQuery);
   const promptObj = {
     prompt: 'Send VCs',
     description: 'Are you sure you want to send VCs to the dApp?',
