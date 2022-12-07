b240393a0893b4b5935697cba9c98a65efd03b07
martines3000 <domajnko.martin@gmail.com>
2022-12-07 08:18:51 +0100

    fix: removes unnecessary  eslint-disable

--- packages/snap/src/rpc/vc/deleteVC.ts
+++ packages/snap/src/rpc/vc/deleteVC.ts
@@ -7,10 +7,8 @@ export async function deleteVC(
   params: ApiParams,
   args: DeleteVCsRequestParams
 ): Promise<boolean[]> {
-  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
   const { id, options } = args || {};
   const { snap } = params;
-  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-member-access
   const store = options?.store;
   const promptObj = {
     prompt: 'Delete VC',
@@ -18,12 +16,9 @@ export async function deleteVC(
   };
 
   if (await snapConfirm(snap, promptObj)) {
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-return
     return await veramoDeleteVC({
       snap,
-      // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
       id,
-      // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
       store,
     });
   }
