2c2391b05ba423642de94765e54cdb5d1d7858aa
martines3000 <domajnko.martin@gmail.com>
2023-01-05 15:16:08 +0100

    fix: fixes docs edit link

--- packages/docs/docusaurus.config.js
+++ packages/docs/docusaurus.config.js
@@ -37,7 +37,7 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/blockchain-lab-um/ssi-snap/packages/docs/edit/develop/',
+            'https://github.com/blockchain-lab-um/ssi-snap/edit/develop/packages/docs/',
         },
         blog: {
           blogSidebarTitle: 'Titles',
