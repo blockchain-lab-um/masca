e9ee7c669b0af84d4a851032b58ec62739560958
martines3000 <domajnko.martin@gmail.com>
2023-03-13 17:59:04 +0100

    fix: reuse navConnect

--- packages/dapp/src/components/Footer/index.tsx
+++ packages/dapp/src/components/Footer/index.tsx
@@ -1,33 +1,11 @@
 import React from 'react';
 import { useRouter } from 'next/router';
-import { shallow } from 'zustand/shallow';
 
-import AddressPopover from '@/components/AddressPopover';
-import ConnectButton from '@/components/ConnectButton';
-import DropdownMenu from '@/components/MethodDropdownMenu';
+import { NavConnection } from '@/components/Navbar/NavConnection';
 import ToggleTheme from '@/components/ToggleTheme';
-import { useGeneralStore, useSnapStore } from '@/utils/stores';
 
 const Footer = () => {
   const router = useRouter();
-
-  const { isConnected, hasMM, hasFlask, address, changeIsConnected } =
-    useGeneralStore(
-      (state) => ({
-        isConnected: state.isConnected,
-        hasMM: state.hasMetaMask,
-        hasFlask: state.isFlask,
-        address: state.address,
-        changeIsConnected: state.changeIsConnected,
-      }),
-      shallow
-    );
-
-  const did = useSnapStore((state) => state.currDID);
-
-  const disconnect = () => {
-    changeIsConnected(false);
-  };
 
   return (
     <div
@@ -35,25 +13,8 @@ const Footer = () => {
         router.pathname === '/' ? 'hidden' : ''
       } mt-auto flex items-center justify-between bg-orange-500/10 px-5 py-2 md:hidden`}
     >
-      {hasFlask && hasMM && (
-        <>
-          {isConnected ? (
-            <span>
-              <div className="m-auto flex items-center justify-center">
-                <AddressPopover
-                  address={address}
-                  did={did}
-                  disconnect={disconnect}
-                />
-                <DropdownMenu />
-              </div>
-            </span>
-          ) : (
-            <ConnectButton />
-          )}
-        </>
-      )}
       <ToggleTheme />
+      <NavConnection />
     </div>
   );
 };
