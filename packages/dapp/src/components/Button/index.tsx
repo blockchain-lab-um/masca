78ff8b62fae2ba3d1586bcee15edbe97059f6235
martines3000 <domajnko.martin@gmail.com>
2023-03-13 15:14:00 +0100

    chore: update button

--- packages/dapp/src/components/Button/index.tsx
+++ packages/dapp/src/components/Button/index.tsx
@@ -16,40 +16,35 @@ type ButtonProps = {
   shadow?: 'sm' | 'md' | 'lg' | 'xl' | '2xl' | 'none' | 'inner' | '';
   onClick?: () => Promise<void> | void;
   children: React.ReactNode;
-  id?: string;
   disabled?: boolean;
   loading?: boolean;
 };
 
 const variants: Record<string, string> = {
   primary:
-    'bg-gradient-to-b from-pink-500 to-orange-500 hover:bg-gradient-to-b hover:opacity-80 text-white rounded-full animated-transition',
+    'bg-gradient-to-b from-pink-500 to-orange-500 hover:bg-gradient-to-b hover:opacity-80 text-white',
   'primary-active':
-    'text-orange-500 border border-orange-500 bg-white hover:text-white hover:bg-orange-500 animated-transition rounded-full',
-  secondary:
-    'bg-navy-blue-500 text-white btn hover:opacity-80  rounded-full animated-transition',
+    'text-orange-500 border border-orange-500 bg-white hover:text-white hover:bg-orange-500',
+  secondary: 'bg-navy-blue-500 text-white btn hover:opacity-80',
   'secondary-active':
-    'text-navy-blue-500 border border-navy-blue-500 hover:text-white hover:bg-navy-blue-500 animated-transition rounded-full',
-  connect:
-    'bg-orange-100 text-orange-500 hover:opacity-80  rounded-full animated-transition',
-  gray: 'bg-gray-200 text-gray-800 hover:opacity-80  rounded-full animated-transition',
+    'text-navy-blue-500 border border-navy-blue-500 hover:text-white hover:bg-navy-blue-500',
+  connect: 'bg-orange-100 text-orange-500 hover:opacity-80',
+  gray: 'bg-gray-200 text-gray-800 hover:opacity-80',
   'white-pink':
-    'bg-white text-gray-800 hover:text-white hover:bg-pink-400 rounded-full dark:bg-pink-300 dark:text-gray-800 dark:hover:bg-pink-200 dark:hover:text-pink-500 animated-transition',
-  white:
-    'bg-white text-orange-500 border border-gray-200 hover:opacity-80  rounded-full animated-transition',
-  warning:
-    'bg-red-500 hover:bg-red-500/90 animated-transition text-white justify-center rounded-full',
+    'bg-white text-gray-800 hover:text-white hover:bg-pink-400 dark:bg-pink-300 dark:text-gray-800 dark:hover:bg-pink-200 dark:hover:text-pink-500',
+  white: 'bg-white text-orange-500 border border-gray-200 hover:opacity-80',
+  warning: 'bg-red-500 hover:bg-red-500/90 text-white justify-center',
 };
 
 const sizes: Record<string, string> = {
-  popup: 'text-sm py-2 px-2.5 max-w-xs',
-  xs: 'text-h5 py-2 px-3 max-w-xs',
-  sm: 'text-h5 py-2 px-5 max-w-xs',
-  md: 'text-h4 py-2 px-7 max-w-xs',
-  lg: 'text-2xl py-2 px-8 font-semibold max-w-xs',
-  xl: 'text-h3 py-2.5 px-9 font-semibold max-w-xs',
-  wd: 'text-h4 py-2.5 px-7 max-w-xs',
-  icon: 'py-2 px-2 max-w-xs',
+  popup: 'text-sm py-2 px-2.5',
+  xs: 'text-h5 py-2 px-3',
+  sm: 'text-h5 py-2 px-5',
+  md: 'text-h4 py-2 px-7',
+  lg: 'text-2xl py-2 px-8 font-semibold',
+  xl: 'text-h3 py-2.5 px-9 font-semibold',
+  wd: 'text-h4 py-2.5 px-7',
+  icon: 'py-2 px-2',
 };
 
 const loaderSizes: Record<string, string> = {
@@ -78,32 +73,30 @@ const Button = ({
   variant = 'primary',
   size = 'md',
   shadow = '',
-  id,
   onClick,
   children,
   disabled = false,
   loading = false,
 }: ButtonProps) => {
   return (
-    <div className="max-w-xs rounded-full bg-gray-100">
+    <div className="rounded-full bg-gray-100">
       <button
         className={`${clsx(
           variants[variant],
           sizes[size],
           `shadow-${shadow}`,
-          'flex items-center gap-x-2'
+          'animated-transition flex max-w-xs items-center gap-x-2 rounded-full'
         )}`}
         onClick={onClick}
-        id={id}
         disabled={disabled}
       >
-        {children}{' '}
+        {children}
         {loading && (
           <div className="flex">
             <div
               className={clsx(
                 loaderSizes[size],
-                'animate-spin rounded-full border-solid border-t-pink-900/0',
+                'ml-1 animate-spin rounded-full border-solid border-t-pink-900/0',
                 loaderColors[variant]
               )}
             ></div>
