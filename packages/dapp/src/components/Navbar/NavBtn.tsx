19edc09f32651565af3f876c8b63c59dc6aeaaff
martines3000 <domajnko.martin@gmail.com>
2023-03-13 14:55:03 +0100

    chore: refactor navbar

--- packages/dapp/src/components/Navbar/NavBtn.tsx
+++ packages/dapp/src/components/Navbar/NavBtn.tsx
@@ -1,19 +1,18 @@
 import React from 'react';
 import Link from 'next/link';
-import { useRouter } from 'next/router';
 
 type NavBtnProps = {
   page: string;
+  pathname: string;
   children: React.ReactNode;
 };
 
-export const NavBtn = ({ page, children }: NavBtnProps) => {
-  const router = useRouter();
+export const NavBtn = ({ page, pathname, children }: NavBtnProps) => {
   return (
     <Link href={page}>
       <button
         className={`nav-btn ${
-          router.pathname === page
+          pathname === page
             ? 'font-bold text-pink-500 after:w-[100%] dark:text-pink-400'
             : 'dark:text-navy-blue-tone/80 text-gray-700'
         }`}
--- packages/dapp/src/components/Navbar/index.tsx
+++ packages/dapp/src/components/Navbar/index.tsx
@@ -2,54 +2,30 @@ import React from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
-import { shallow } from 'zustand/shallow';
 
-import AddressPopover from '@/components/AddressPopover';
-import ConnectButton from '@/components/ConnectButton';
 import MenuPopover from '@/components/MenuPopover';
-import DropdownMenu from '@/components/MethodDropdownMenu';
 import ToggleTheme from '@/components/ToggleTheme';
 import { BASE_PATH } from '@/utils/constants';
-import { useGeneralStore, useSnapStore } from '@/utils/stores';
 import { NavBtn } from './NavBtn';
+import { NavConnection } from './NavConnection';
 
 export default function Navbar() {
   const router = useRouter();
-  const did = useSnapStore((state) => state.currDID);
-
-  const { isConnected, hasMM, hasFlask, address, changeIsConnected } =
-    useGeneralStore(
-      (state) => ({
-        isConnected: state.isConnected,
-        hasMM: state.hasMetaMask,
-        hasFlask: state.isFlask,
-        address: state.address,
-        changeIsConnected: state.changeIsConnected,
-      }),
-      shallow
-    );
-
-  const setVcs = useSnapStore((state) => state.changeVcs);
-
-  const disconnect = () => {
-    setVcs([]);
-    changeIsConnected(false);
-  };
 
   return (
     <div className="flex items-center justify-between">
       <Link href="/">
         <button>
           <div className="flex">
-            <div className="relative h-[24px] w-[24px] rounded-full object-center dark:hidden sm:h-[36px] sm:w-[36px] lg:h-[46px] lg:w-[46px] xl:h-[48px] xl:w-[50px]">
+            <div className="relative h-[24px] w-[24px] rounded-full object-center sm:h-[36px] sm:w-[36px] lg:h-[46px] lg:w-[46px] xl:h-[48px] xl:w-[50px]">
               <Image
+                className="dark:hidden"
                 src={`${BASE_PATH}/images/ssi_icon_b.png`}
                 alt="Masca Logo"
                 fill={true}
               />
-            </div>
-            <div className="relative hidden h-[24px] w-[24px] rounded-full object-center dark:block sm:h-[36px] sm:w-[36px] lg:h-[46px] lg:w-[46px] xl:h-[48px] xl:w-[50px]">
               <Image
+                className="hidden dark:block"
                 src={`${BASE_PATH}/images/ssi_icon_w.png`}
                 alt="Masca Logo"
                 fill={true}
@@ -63,36 +39,21 @@ export default function Navbar() {
       </Link>
       {router.pathname !== '/' && (
         <div className="my-auto mx-2 flex">
-          <NavBtn page="/">Home</NavBtn>
-          <NavBtn page="/dashboard">Dashboard</NavBtn>
-          <NavBtn page="/settings">Settings</NavBtn>
+          <NavBtn page="/" pathname={router.pathname}>
+            Home
+          </NavBtn>
+          <NavBtn page="/dashboard" pathname={router.pathname}>
+            Dashboard
+          </NavBtn>
+          <NavBtn page="/settings" pathname={router.pathname}>
+            Settings
+          </NavBtn>
           <MenuPopover />
         </div>
       )}
       <div className="hidden md:block">
         <div className="flex justify-between">
-          {hasMM && hasFlask && (
-            <>
-              {router.pathname !== '/' && (
-                <>
-                  {isConnected ? (
-                    <div className="flex items-center justify-center">
-                      <DropdownMenu />
-                      <AddressPopover
-                        address={address}
-                        did={did}
-                        disconnect={disconnect}
-                      />
-                    </div>
-                  ) : (
-                    <div className="m-auto flex">
-                      <ConnectButton />
-                    </div>
-                  )}
-                </>
-              )}
-            </>
-          )}
+          {router.pathname !== '/' && <NavConnection />}
           <ToggleTheme />
         </div>
       </div>
