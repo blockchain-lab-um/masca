300c0445fcfc8b20503b8c7338334aee83dbfb90
martines3000 <domajnko.martin@gmail.com>
2023-03-17 13:32:54 +0100

    feat: esm ssi-snap-types

--- .eslintrc.js
+++ .eslintrc.js
@@ -41,7 +41,7 @@ module.exports = {
           '**/*.spec.ts',
           '**/*.e2e-spec.ts',
           '**/webpack.config.ts',
-          'tsup.config.ts',
+          '**/tsup.config.ts',
         ],
       },
     ],
