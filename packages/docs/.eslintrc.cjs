6f27d71d063649d268dfa84371a5f27b913224ee
martines3000 <domajnko.martin@gmail.com>
2023-03-17 15:25:45 +0100

    fix: docs

--- packages/docs/.eslintrc.cjs
+++ packages/docs/.eslintrc.cjs
@@ -1,6 +1,6 @@
 module.exports = {
   root: true,
-  extends: ['../../.eslintrc.js'],
+  extends: ['../../.eslintrc.cjs'],
   plugins: ['import'],
   parserOptions: {
     tsconfigRootDir: __dirname,
