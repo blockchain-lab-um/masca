a1ec8515350e2a2d6e39a4ead6dfc4ddefd4aa9b
Martin Domajnko <domajnko.martin@gmail.com>
2023-04-21 11:13:12 +0200

    fix: remove broken docusaurus config

--- packages/docs/docusaurus.config.js
+++ packages/docs/docusaurus.config.js
@@ -19,7 +19,6 @@ const config = {
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'blockchain-lab-um', // Usually your GitHub org/user name.
   projectName: 'masca-docs', // Usually your repo name.
-  projectName: 'masca-docs', // Usually your repo name.
   customFields: {},
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
