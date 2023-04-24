3fb10a67610954aba73eb0942b948dbd4513407d
Urban Vidovič <urbanfoundit@gmail.com>
2023-04-24 17:53:15 +0200

    chore: delete remaining did ebsi files, update snap's package.json
    Signed-off-by: Urban Vidovič <urbanfoundit@gmail.com>

---
+++ libs/did-provider-ebsi/src/index.ts
@@ -0,0 +1,9 @@
+/**
+ * Provides `did:ebsi` {@link @veramo/did-provider-ebsi#EbsiDIDProvider | identifier provider } for the
+ * {@link @veramo/did-manager#DIDManager}
+ *
+ * @packageDocumentation
+ */
+export { EbsiDIDProvider } from './ebsiDidProvider.js';
+export { ebsiDidResolver } from './ebsiDidResolver.js';
+export * from './types/ebsiProviderTypes.js';
