e405d3d855e1b43113e3c67c8a039b023dddeb92
Tadej <tadej.podrekar@gmail.com>
2023-05-30 14:12:42 +0200

    fix: pr comment

--- libs/utils/src/multicodecs.ts
+++ libs/utils/src/multicodecs.ts
@@ -5,6 +5,7 @@ export type CodecName =
   | 'ed25519-priv'
   | 'ed25519-pub'
   | 'jwk_jcs-pub'
+  | 'x25519-priv'
   | 'x25519-pub'
   | 'p256-priv'
   | 'p256-pub';
@@ -15,6 +16,7 @@ export const MULTICODECS: Record<CodecName, Uint8Array> = {
   'ed25519-priv': new Uint8Array([128, 38]),
   'ed25519-pub': new Uint8Array([237, 1]),
   'jwk_jcs-pub': new Uint8Array([235, 81]),
+  'x25519-priv': new Uint8Array([128, 40]),
   'x25519-pub': new Uint8Array([238, 1]),
   'p256-priv': new Uint8Array([128, 39]),
   'p256-pub': new Uint8Array([235, 7]),
@@ -26,6 +28,7 @@ export const MULTICODECSCODES: Record<CodecName, string> = {
   'ed25519-priv': '0x1300',
   'ed25519-pub': '0xed',
   'jwk_jcs-pub': '0xeb51',
+  'x25519-priv': '0x1302',
   'x25519-pub': '0xec',
   'p256-priv': '0x1306',
   'p256-pub': '0x1200',
@@ -37,6 +40,7 @@ export const keyLengthsMap: Record<CodecName, number> = {
   'ed25519-priv': 32,
   'ed25519-pub': 32,
   'jwk_jcs-pub': 32,
+  'x25519-priv': 32,
   'x25519-pub': 32,
   'p256-priv': 32,
   'p256-pub': 65,
