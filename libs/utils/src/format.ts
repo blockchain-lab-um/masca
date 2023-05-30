f19c406e02287b3f6da7d704d70fafdb3958d7a7
Tadej <tadej.podrekar@gmail.com>
2023-05-30 14:14:20 +0200

    fix: remove unused function and constants

--- libs/utils/src/format.ts
+++ libs/utils/src/format.ts
@@ -2,30 +2,7 @@ import { varint } from 'multiformats';
 import { base58btc } from 'multiformats/bases/base58';
 import { publicKeyConvert } from 'secp256k1';
 
-import { CodecName, MULTICODECS, MULTICODECSCODES } from './multicodecs.js';
-
-/**
- * Prefix a buffer with a multicodec-packed.
- *
- * @param {CodecName} multicodec
- * @param {Uint8Array} data
- *
- * @returns {Uint8Array}
- */
-export const addMulticodecPrefix = (
-  multicodec: CodecName,
-  data: Uint8Array
-): Uint8Array => {
-  let prefix;
-
-  if (MULTICODECS[multicodec]) {
-    prefix = Buffer.from(MULTICODECS[multicodec]);
-  } else {
-    throw new Error('multicodec not recognized');
-  }
-
-  return Buffer.concat([prefix, data], prefix.length + data.length);
-};
+import { CodecName, MULTICODECSCODES } from './multicodecs.js';
 
 export const encodePublicKey = (
   pubKeyBytes: Uint8Array,
@@ -63,6 +40,7 @@ export function uint8ArrayToHex(arr: Uint8Array) {
 export function hexToUint8Array(str: string): Uint8Array {
   return new Uint8Array(Buffer.from(str, 'hex'));
 }
+
 export function getCompressedPublicKey(publicKey: string): string {
   return uint8ArrayToHex(
     publicKeyConvert(hexToUint8Array(publicKey.split('0x')[1]), true)
--- libs/utils/src/multicodecs.ts
+++ libs/utils/src/multicodecs.ts
@@ -10,18 +10,6 @@ export type CodecName =
   | 'p256-priv'
   | 'p256-pub';
 
-export const MULTICODECS: Record<CodecName, Uint8Array> = {
-  'secp256k1-priv': new Uint8Array([129, 38]),
-  'secp256k1-pub': new Uint8Array([231, 1]),
-  'ed25519-priv': new Uint8Array([128, 38]),
-  'ed25519-pub': new Uint8Array([237, 1]),
-  'jwk_jcs-pub': new Uint8Array([235, 81]),
-  'x25519-priv': new Uint8Array([128, 40]),
-  'x25519-pub': new Uint8Array([238, 1]),
-  'p256-priv': new Uint8Array([128, 39]),
-  'p256-pub': new Uint8Array([235, 7]),
-};
-
 export const MULTICODECSCODES: Record<CodecName, string> = {
   'secp256k1-priv': '0x1301',
   'secp256k1-pub': '0xe7',
