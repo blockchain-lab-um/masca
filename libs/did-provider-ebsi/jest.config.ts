118256163e95977eda126e4150da6ac29630c7a0
Urban Vidovič <urbanfoundit@gmail.com>
2023-06-05 15:08:58 +0200

    chore: wip - ebsi tests
    Signed-off-by: Urban Vidovič <urbanfoundit@gmail.com>

--- libs/did-provider-ebsi/jest.config.ts
+++ libs/did-provider-ebsi/jest.config.ts
@@ -1,27 +1,25 @@
 export default {
-  clearMocks: true,
-  collectCoverage: true,
+  clearMocks: false,
+  collectCoverage: false,
   coverageDirectory: 'coverage',
   coveragePathIgnorePatterns: ['/node_modules/', 'tests'],
-  moduleFileExtensions: ['js', 'mjs', 'cjs', 'jsx', 'ts'],
-  extensionsToTreatAsEsm: ['.ts'],
+  coverageProvider: 'v8',
+  moduleFileExtensions: ['js', 'json', 'ts', 'mjs', 'cjs'],
+  testEnvironment: 'node',
   testRegex: '.*\\.spec\\.ts$',
+  setupFilesAfterEnv: ['jest-extended/all'],
+  globals: {},
+  transform: {
+    '^.+\\.(t|j)sx?$': 'babel-jest',
+  },
   moduleNameMapper: {
     '^(\\.{1,2}/.*)\\.js$': '$1',
+    // Waiting for this issue to be implemented:
+    // https://github.com/facebook/jest/issues/9771
+    '^multiformats/(.*)$': '<rootDir>/node_modules/multiformats/src/$1.js',
+    '^multiformats$': '<rootDir>/node_modules/multiformats/src/index.js',
   },
-  transform: {
-    '^.+\\.(t|j)sx?$': [
-      '@swc/jest',
-      {
-        jsc: {
-          target: 'es2022',
-          parser: {
-            syntax: 'typescript',
-            dynamicImport: true,
-          },
-        },
-      },
-    ],
-  },
+  extensionsToTreatAsEsm: ['.ts'],
   transformIgnorePatterns: ['/node_modules/(?!@veramo)/'],
+  testTimeout: 120000,
 };
