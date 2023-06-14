4d40d22385bfb1f0115326ba1e3ee0ba6154daad
Martin Domajnko <domajnko.martin@gmail.com>
2023-06-14 11:54:04 +0200

    fix: fixes docker build

--- apps/oidc/verifier/build-docker.sh
+++ apps/oidc/verifier/build-docker.sh
@@ -2,14 +2,15 @@
 
 projects="\
 @blockchain-lab-um/masca-docs,\
+@blockchain-lab-um/masca-connector,\
+@blockchain-lab-um/masca-types,\
 @blockchain-lab-um/dapp,\
 @blockchain-lab-um/utils,\
-@blockchain-lab-um/masca-types,\
-@blockchain-lab-um/masca-connector,\
-@blockchain-lab-um/oidc-issuer,\
-@blockchain-lab-um/oidc-verifier,\
 @blockchain-lab-um/oidc-rp-plugin,\
-@blockchain-lab-um/did-provider-key\
+@blockchain-lab-um/oidc-types,\
+@blockchain-lab-um/did-provider-key,\
+@blockchain-lab-um/oidc-issuer,\
+@blockchain-lab-um/oidc-verifier\
 "
 
 pnpm nx run-many \
