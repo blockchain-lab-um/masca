52bd268c56cf5a02d8ad8637d36f583357cf34f9
Martin Domajnko <domajnko.martin@gmail.com>
2023-06-19 17:13:12 +0200

    fix: update masca version in connector

--- packages/connector/src/index.ts
+++ packages/connector/src/index.ts
@@ -32,7 +32,7 @@ export async function enableMasca(
 ): Promise<Result<Masca>> {
   const {
     snapId = defaultSnapOrigin,
-    version = '^0.1.0',
+    version = 'latest',
     supportedMethods = ['did:ethr'],
   } = snapInstallationParams;
 
