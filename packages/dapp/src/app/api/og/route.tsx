b709eaa072fe77f8d0d66329fed53e0c8632ee2d
Martin Domajnko <domajnko.martin@gmail.com>
2023-06-20 17:17:54 +0200

    fix: reduce og api route size

--- packages/dapp/src/app/api/og/route.tsx
+++ packages/dapp/src/app/api/og/route.tsx
@@ -65,8 +65,8 @@ export async function GET(req: Request) {
               <div tw="flex flex-2 flex-col items-center justify-center">
                 <img
                   alt="Masca logo"
-                  width={333}
-                  src="data:image/svg+xml,%3Csvg width='415' height='364' viewBox='0 0 415 364' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M363.619 182c0 100.516-81.399 182-181.809 182C81.399 364 0 282.516 0 182S81.399 0 181.81 0c100.41 0 181.809 81.484 181.809 182Z' fill='url(%23a)'/%3E%3Cpath d='M146.238 146.391c0-10.925 8.848-19.782 19.762-19.782h229.238c10.914 0 19.762 8.857 19.762 19.782v106.826c0 10.926-8.848 19.783-19.762 19.783H166c-10.914 0-19.762-8.857-19.762-19.783V146.391Z' fill='%23000'/%3E%3Cdefs%3E%3ClinearGradient id='a' x1='207.5' y1='0' x2='207.5' y2='364' gradientUnits='userSpaceOnUse'%3E%3Cstop stop-color='%23FE3D67'/%3E%3Cstop offset='1' stop-color='%23FF7131'/%3E%3C/linearGradient%3E%3C/defs%3E%3C/svg%3E"
+                  width={256}
+                  src="https://masca.io/images/masca_black.svg"
                 />
               </div>
             </div>
