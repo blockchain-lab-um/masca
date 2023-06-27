e28d733e5178310b0820838c75977bc8412f4d31
Martin Domajnko <domajnko.martin@gmail.com>
2023-06-27 22:30:44 +0200

    chore: remove unused code from snapMock

--- packages/snap/tests/helpers/snapMock.ts
+++ packages/snap/tests/helpers/snapMock.ts
@@ -19,8 +19,6 @@ interface SnapManageState {
 export class SnapMock implements ISnapMock {
   private snapState: MascaState | null = null;
 
-  // private snap: Wallet = new Wallet(privateKey);
-
   private snapManageState(params: SnapManageState): MascaState | null {
     if (!params) {
       return null;
@@ -47,11 +45,6 @@ export class SnapMock implements ISnapMock {
         return '0x0000000000000000000000000000000000000000000000000000000000000000';
     }
   }
-
-  //   private async snapPersonalSign(data: string[]): Promise<string> {
-  //     const signature = await this.snap.signMessage(data[0]);
-  //     return signature;
-  //   }
 
   private async snapEthCall(data: any[]): Promise<string> {
     const apiKey = 'NRFBwig_CLVL0WnQLY3dUo8YkPmW-7iN';
@@ -94,11 +87,6 @@ export class SnapMock implements ISnapMock {
       .mockImplementation((params: unknown) =>
         this.snapManageState(params as SnapManageState)
       ),
-    // personal_sign: jest
-    //   .fn()
-    //   .mockImplementation(async (data: unknown) =>
-    //     this.snapPersonalSign(data as string[])
-    //   ),
     eth_call: jest
       .fn()
       .mockImplementation(async (data: unknown) =>
@@ -109,20 +97,11 @@ export class SnapMock implements ISnapMock {
       .mockImplementation(async (data: unknown) =>
         this.snapEthLogs(data as any[])
       ),
-    // eth_signTypedData_v4: jest
-    //   .fn()
-    //   .mockImplementation((...params: unknown[]) => {
-    //     const { domain, types, message } = JSON.parse(params[1] as string);
 
-    //     delete types.EIP712Domain;
-
-    //     return this.snap.signTypedData(domain, types, message);
-    //   }),
   };
 
   request<T>(args: RequestArguments): Promise<Maybe<T>> {
     const { method, params } = args;
-    // @ts-expect-error Args params won't cause an issue
     // eslint-disable-next-line
     return this.rpcMocks[method](params);
   }
