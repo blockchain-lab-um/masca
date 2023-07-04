744b2e651dda15e7b44af894294692925c9a4964
martines3000 <domajnko.martin@gmail.com>
2023-07-04 19:17:48 +0200

    chore: bump version of snap to 0.3.1

--- packages/connector/src/index.ts
+++ packages/connector/src/index.ts
@@ -32,7 +32,7 @@ export async function enableMasca(
 ): Promise<Result<Masca>> {
   const {
     snapId = defaultSnapOrigin,
-    version = '0.3.0',
+    version = '0.3.1',
     supportedMethods = ['did:ethr'],
   } = snapInstallationParams;
 
