8f2f721de78afeb65fcea96826c3d96f7efa7649
andyv09 <andraz.vrecko@gmail.com>
2023-07-12 14:18:25 +0200

    feat: add option to change networks for polygonid

--- packages/dapp/src/utils/networks.ts
+++ packages/dapp/src/utils/networks.ts
@@ -3,11 +3,14 @@ export const NETWORKS: Record<string, string> = {
   '0xaa36a7': 'Sepolia Testnet',
   '0x5': 'Goerli Testnet',
   '0x89': 'Polygon',
+  '0x13881': 'Polygon Mumbai',
 };
 
 export const NETWORKS_BY_DID: Record<string, string[]> = {
   'did:ethr': ['*'],
   'did:pkh': ['0x1', '0x89'],
+  'did:polygon': ['0x1', '0x5', '0x89', '0x13881'],
+  'did:iden3': ['0x1', '0x5', '0x89', '0x13881'],
 };
 
 export function getAvailableNetworksList(method: string): string[] {
