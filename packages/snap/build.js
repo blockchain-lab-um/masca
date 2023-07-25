d178b8a75abe938aee9e9b2047d25bd7c352ed4f
Martin Domajnko <35891136+martines3000@users.noreply.github.com>
2023-07-25 10:37:44 +0200

    fix: resolves build and test issues (#354)

--- packages/snap/build.js
+++ packages/snap/build.js
@@ -1,7 +1,10 @@
 const esbuild = require('esbuild');
 const plugin = require('node-stdlib-browser/helpers/esbuild/plugin');
-const stdLibBrowser = require('node-stdlib-browser');
-const path = require('path');
+let stdLibBrowser = require('node-stdlib-browser');
+
+stdLibBrowser = {
+  ...stdLibBrowser,
+};
 
 console.log('Building snap with esbuild...');
 
@@ -15,15 +18,15 @@ esbuild.build({
   treeShaking: true,
   tsconfig: 'tsconfig.build.json',
   alias: {
-    '@0xpolygonid/js-sdk': '@0xpolygonid/js-sdk/dist/esm/index.js',
+    // "@0xpolygonid/js-sdk":'../../node_modules/.pnpm/github.com+0xPolygonID+js-sdk@b50447564b642c3835c811bd32e1350eb189578c_xr65kx4diede7av7icn3kqhvj4/node_modules/@0xpolygonid/js-sdk/dist/esm/index.js'
   },
   plugins: [plugin(stdLibBrowser)],
+  inject: [require.resolve('node-stdlib-browser/helpers/esbuild/shim')],
   define: {
     global: 'global',
     process: 'process',
     Buffer: 'Buffer',
   },
-  inject: [require.resolve('node-stdlib-browser/helpers/esbuild/shim')],
 });
 
 console.log('Finished building snap...');
