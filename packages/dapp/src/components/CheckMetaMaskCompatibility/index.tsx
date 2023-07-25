dd9900a05f01e1475cca7ea57971dacef3c6ac6b
martines3000 <domajnko.martin@gmail.com>
2023-07-25 11:48:53 +0200

    chore: bump masca version in enableMasca

--- packages/dapp/src/components/CheckMetaMaskCompatibility/index.tsx
+++ packages/dapp/src/components/CheckMetaMaskCompatibility/index.tsx
@@ -116,7 +116,7 @@ const CheckMetaMaskCompatibility = () => {
   const enableMascaHandler = async () => {
     const enableResult = await enableMasca(address, {
       snapId,
-      version: '0.3.1',
+      version: '0.3.2',
     });
     if (isError(enableResult)) {
       // FIXME: This error is shown as [Object object]
