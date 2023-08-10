04af66adf924b7456e3c11395712c16f5091ca5b
martines3000 <domajnko.martin@gmail.com>
2023-08-10 17:49:10 +0200

    ci: fixes issues with version-beta.sh

--- scripts/changesets/version-beta.sh
+++ scripts/changesets/version-beta.sh
@@ -7,7 +7,7 @@ pnpm changeset version && \
   pnpm install --lockfile-only && \
   pnpm build && \
   git add . && \
-  git commit -m \"chore: update versions\" && \
+  git commit -m "chore: update versions" && \
   echo "Successfully updated beta version!" || \
   echo "Failed to update beta version!"
 
