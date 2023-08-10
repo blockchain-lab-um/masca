37059a003e3c6e4cfc3d0219361fde24e1ef0dfb
martines3000 <domajnko.martin@gmail.com>
2023-08-10 23:59:45 +0200

    ci: fixes version stable.sh

--- scripts/changesets/version-stable.sh
+++ scripts/changesets/version-stable.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 
+# Checkout or create a new branch
+
 echo "Updating stable version..."
 
-node scripts/changesets/version-stable.mjs $1 && \
+
+# First we exit rc mode and then we version the packages
+pnpm changeset pre exit && \
+  node scripts/changesets/version-stable.mjs $1 && \
   node scripts/changesets/update-snap-version.mjs && \
   pnpm install --lockfile-only && \
   pnpm build && \
-  git status && \
-  git add . && \
+  git add --all && \
   git commit -m "chore: update versions" && \
   echo "Successfully updated stable version!" || \
   echo "Failed to update stable version!"
+
