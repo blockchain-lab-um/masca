2d5851a9661bc12e4f2fa2553475577bf3e2df3f
martines3000 <domajnko.martin@gmail.com>
2023-08-10 23:55:36 +0200

    ci: update scripts

--- scripts/changesets/version-stable.sh
+++ scripts/changesets/version-stable.sh
@@ -6,7 +6,8 @@ node scripts/changesets/version-stable.mjs $1 && \
   node scripts/changesets/update-snap-version.mjs && \
   pnpm install --lockfile-only && \
   pnpm build && \
-  git add --all && \
+  git status && \
+  git add . && \
   git commit -m "chore: update versions" && \
   echo "Successfully updated stable version!" || \
   echo "Failed to update stable version!"
