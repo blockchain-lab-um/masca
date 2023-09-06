8414c970a55fd982f1f152357b6cf59cbf840ea2
andyv09 <andraz.vrecko@gmail.com>
2023-09-06 15:37:16 +0200

    fix: update Masca feature card

--- packages/dapp/src/components/ConnectedProvider/index.tsx
+++ packages/dapp/src/components/ConnectedProvider/index.tsx
@@ -1,7 +1,12 @@
 'use client';
 
 import React from 'react';
-import { PlusCircleIcon } from '@heroicons/react/24/solid';
+import {
+  CreditCardIcon,
+  GlobeAltIcon,
+  LockClosedIcon,
+  PlusCircleIcon,
+} from '@heroicons/react/24/solid';
 import { useTranslations } from 'next-intl';
 
 import { useGeneralStore } from '@/stores';
@@ -25,59 +30,89 @@ const ConnectedProvider = ({ children }: ConnectedProviderProps) => {
         <div className="text-h3 dark:text-navy-blue-50 text-gray-900">
           Connect Wallet & Install Masca
         </div>
-        <div className="dark:border-navy-blue-500 mt-8 rounded-3xl border border-gray-500 px-6 py-4">
+        <div className="dark:border-navy-blue-500 mt-8 rounded-3xl border border-gray-500 px-12 py-8 md:min-w-[40em]">
           <div className="flex items-center gap-x-6">
             <div className="hidden sm:block">
               <MascaLogo />
             </div>
             <div>
-              <div className="text-h3 dark:text-navy-blue-50 text-gray-900">
+              <div className="text-h3 dark:text-navy-blue-50 font-ubuntu text-gray-900">
                 Masca
               </div>
-              <div className="text-h4 dark:text-navy-blue-300 mt-2 text-gray-600">
+              <div className="text-h4 dark:text-navy-blue-300 font-ubuntu mt-2 text-gray-600">
                 Manage Decentralized Identity in MetaMask
               </div>
             </div>
           </div>
           <hr className="mt-4" />
-          <ul>
-            <li className="mt-12">
-              <div className=" flex items-center gap-x-6">
-                <PlusCircleIcon className="h-6 w-6" />
-                <div className="dark:text-navy-blue-100 text-2xl font-medium text-gray-900 ">
-                  Manage you Decentralized Identifiers
+          <div className="flex justify-center">
+            <ul className="flex flex-col items-start text-justify text-xl tracking-normal">
+              <li className="mt-12">
+                <div className=" flex items-center gap-x-4">
+                  <LockClosedIcon className="dark:text-orange-accent-dark h-8 w-8 text-pink-500" />
+                  <div className="dark:text-navy-blue-50 font-ubuntu text-2xl font-medium text-gray-900 ">
+                    Own your identity
+                  </div>
                 </div>
-              </div>
-              <div className="dark:text-navy-blue-200 mt-4 max-w-md text-gray-800">
-                Create and manage your decentralized identifiers. Masca supports
-                multiple popular DID methods.
-              </div>
-            </li>
-            <li className="mt-8">
-              <div className=" flex items-center gap-x-6">
-                <PlusCircleIcon className="h-6 w-6" />
-                <div className="dark:text-navy-blue-100 text-2xl font-medium text-gray-900 ">
-                  Manage your Credentials
+                <div className="dark:text-navy-blue-200 mt-4 max-w-md text-gray-700">
+                  Imagine your{' '}
+                  <span className="dark:text-navy-blue-100 font-bold text-gray-800">
+                    Ethereum account{' '}
+                  </span>
+                  as a passport in the digital world. With just a few clicks,
+                  transform your existing account into a{' '}
+                  <span className="dark:text-navy-blue-100 font-bold text-gray-800">
+                    universal digital identity
+                  </span>
+                  , trusted everywhere.
                 </div>
-              </div>
-              <div className="dark:text-navy-blue-200 mt-4 max-w-md text-gray-800">
-                Store Verifiable Credentials in Masca or Onchain & use them to
-                create Verifiable Presentation.
-              </div>
-            </li>
-            <li className="mt-8">
-              <div className=" flex items-center gap-x-6">
-                <PlusCircleIcon className="h-6 w-6" />
-                <div className="dark:text-navy-blue-100 text-2xl font-medium text-gray-900 ">
-                  Receive and share Credentials
+              </li>
+              <li className="mt-12">
+                <div className=" flex items-center gap-x-4">
+                  <CreditCardIcon className="dark:text-orange-accent-dark h-8 w-8 text-pink-500" />
+                  <div className="dark:text-navy-blue-50 font-ubuntu text-2xl font-medium text-gray-900 ">
+                    Manage & share Credentials securely
+                  </div>
                 </div>
-              </div>
-              <div className="dark:text-navy-blue-200 mt-4 max-w-md text-gray-800">
-                Masca supports Polygon ID & OID4VC flows to enable secure data
-                sharing between entities.
-              </div>
-            </li>
-          </ul>
+                <div className="dark:text-navy-blue-200 mt-4 max-w-md text-gray-700">
+                  Store everything from your driving license to your job
+                  credentials safely and securely in one place. Choose to save
+                  them on{' '}
+                  <span className="dark:text-navy-blue-100 font-bold text-gray-800">
+                    Ceramic Network
+                  </span>{' '}
+                  or keep them privately in your
+                  <span className="dark:text-navy-blue-100 font-bold text-gray-800">
+                    {' '}
+                    MetaMask wallet
+                  </span>
+                  . These credentials can be shared with anyone, and they can be
+                  verified using blockchain technology.
+                </div>
+              </li>
+              <li className="mt-12">
+                <div className=" flex items-center gap-x-4">
+                  <GlobeAltIcon className="dark:text-orange-accent-dark h-8 w-8 text-pink-500" />
+                  <div className="dark:text-navy-blue-50 font-ubuntu max-w-[15em] text-start text-2xl font-medium text-gray-900 ">
+                    Universal Compatibility with Identity Flows
+                  </div>
+                </div>
+                <div className="dark:text-navy-blue-200 mt-4 max-w-md text-gray-700">
+                  Easily use your digital identity across different platforms
+                  and services with no extra setup. Masca supports popular
+                  identity systems like{' '}
+                  <span className="dark:text-navy-blue-100 font-bold text-gray-800">
+                    Polygon ID
+                  </span>{' '}
+                  and{' '}
+                  <span className="dark:text-navy-blue-100 font-bold text-gray-800">
+                    OID4VC
+                  </span>
+                  , making your life easier and more secure.
+                </div>
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
     </div>
