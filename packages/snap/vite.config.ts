fe204fc0f067471d6d81ff9b33ee3a1f61d33fdd
Martin Domajnko <domajnko.martin@gmail.com>
2023-10-05 14:28:15 +0200

    fix: update vitest config

--- packages/snap/vite.config.ts
+++ packages/snap/vite.config.ts
@@ -5,7 +5,9 @@ import { defineConfig } from 'vitest/config';
 export default defineConfig({
   plugins: [tsconfigPaths()],
   test: {
+    include: ['tests/e2e/**/*.spec.ts', 'tests/unit/**/*.spec.ts'],
     silent: true,
+    cache: false,
     environment: 'node', // or 'happy-dom', 'jsdom'
     server: {
       deps: {
@@ -15,6 +17,17 @@ export default defineConfig({
     },
     setupFiles: ['./tests/globalSetup.ts'],
     testTimeout: 15000,
+    poolOptions: {
+      threads: {
+        minThreads: 1,
+        maxThreads: 4,
+      },
+    },
     logHeapUsage: true,
+    coverage: {
+      enabled: true,
+      provider: 'v8',
+      clean: true,
+    },
   },
 });
