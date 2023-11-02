54387f457b98b18636ae9388549faf0c00d998c5
Martin Domajnko <35891136+martines3000@users.noreply.github.com>
2023-11-02 16:27:50 +0100

    chore: update deps (#515)

--- libs/did-provider-key/vite.config.ts
+++ libs/did-provider-key/vite.config.ts
@@ -8,18 +8,10 @@ export default defineConfig({
   test: {
     watch: false,
     pool: 'forks',
-    include: process.env.CRON
-      ? ['tests/cron/**/*.spec.ts']
-      : ['tests/e2e/**/*.spec.ts', 'tests/unit/**/*.spec.ts'],
+    include: ['tests/agent.spec.ts'],
     silent: true,
     cache: false,
     environment: 'node', // or 'happy-dom', 'jsdom'
-    server: {
-      deps: {
-        fallbackCJS: true,
-        external: ['did-resolver', 'did-jwt-vc'],
-      },
-    },
     setupFiles: ['./tests/globalSetup.ts'],
     testTimeout: 60000,
     poolOptions: {
