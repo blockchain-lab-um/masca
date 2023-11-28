3952981e3d1404eee62a54a64358417c10482204
Andraz <69682837+andyv09@users.noreply.github.com>
2023-11-28 14:55:24 +0100

    feat: VC filter (#521)
    Co-authored-by: Urban <urbanfoundit@gmail.com>

--- packages/dapp/src/stores/tableStore.ts
+++ packages/dapp/src/stores/tableStore.ts
@@ -1,18 +1,42 @@
-import { type QueryCredentialsRequestResult } from '@blockchain-lab-um/masca-connector';
+import {
+  AvailableCredentialStores,
+  type QueryCredentialsRequestResult,
+} from '@blockchain-lab-um/masca-connector';
 import { ColumnFiltersState } from '@tanstack/react-table';
 import { shallow } from 'zustand/shallow';
 import { createWithEqualityFn } from 'zustand/traditional';
+
+interface DataStore {
+  dataStore: AvailableCredentialStores;
+  selected: boolean;
+}
+
+interface CredentialType {
+  type: string;
+  selected: boolean;
+}
+
+interface Ecosystem {
+  ecosystem: 'ebsi' | 'polygonid' | 'other';
+  selected: boolean;
+}
 
 interface TableStore {
   globalFilter: string;
   columnFilters: ColumnFiltersState;
   selectedVCs: QueryCredentialsRequestResult[];
   cardView: boolean;
+  dataStores: DataStore[];
+  ecosystems: Ecosystem[];
+  credentialTypes: CredentialType[];
 
   setGlobalFilter: (globalFilter: string) => void;
   setColumnFilters: (columnFilters: ColumnFiltersState) => void;
   setSelectedVCs: (selectedVCs: QueryCredentialsRequestResult[]) => void;
   setCardView: (view: boolean) => void;
+  setDataStores: (dataStores: DataStore[]) => void;
+  setCredentialTypes: (credentialTypes: CredentialType[]) => void;
+  setEcosystems: (ecosystems: Ecosystem[]) => void;
 }
 
 export const tableStoreInitialState = {
@@ -20,6 +44,16 @@ export const tableStoreInitialState = {
   columnFilters: [{ id: 'data_store', value: ['snap'] }],
   selectedVCs: [],
   cardView: true,
+  dataStores: [
+    { dataStore: 'snap', selected: true } as DataStore,
+    { dataStore: 'ceramic', selected: true } as DataStore,
+  ],
+  ecosystems: [
+    { ecosystem: 'ebsi', selected: true } as Ecosystem,
+    { ecosystem: 'polygonid', selected: true } as Ecosystem,
+    { ecosystem: 'other', selected: true } as Ecosystem,
+  ],
+  credentialTypes: [],
 };
 
 export const useTableStore = createWithEqualityFn<TableStore>()(
@@ -32,6 +66,11 @@ export const useTableStore = createWithEqualityFn<TableStore>()(
     setCardView: (cardView: boolean) => set({ cardView }),
     setSelectedVCs: (selectedVCs: QueryCredentialsRequestResult[]) =>
       set({ selectedVCs }),
+    setDataStores: (dataStores: DataStore[]) => set({ dataStores }),
+    setCredentialTypes: (credentialTypes: CredentialType[]) =>
+      set({ credentialTypes }),
+
+    setEcosystems: (ecosystems: Ecosystem[]) => set({ ecosystems }),
   }),
   shallow
 );
--- packages/dapp/tailwind.config.cjs
+++ packages/dapp/tailwind.config.cjs
@@ -153,5 +153,5 @@ module.exports = {
       },
     },
   },
-  plugins: [require('tailwind-scrollbar')],
+  plugins: [require('tailwind-scrollbar')({ nocompatible: true })],
 };
