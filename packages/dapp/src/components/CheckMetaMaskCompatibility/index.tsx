570d888df64549975e5a4f6b9d942b0ace57b5ef
martines3000 <domajnko.martin@gmail.com>
2023-12-18 23:23:09 +0100

    chore: fix

--- packages/dapp/src/components/CheckMetaMaskCompatibility/index.tsx
+++ packages/dapp/src/components/CheckMetaMaskCompatibility/index.tsx
@@ -11,12 +11,12 @@ import detectEthereumProvider from '@metamask/detect-provider';
 import { useGeneralStore, useMascaStore } from '@/stores';
 import { useAuthStore } from '@/stores/authStore';
 
-// const snapId =
-//   process.env.USE_LOCAL === 'true'
-//     ? 'local:http://localhost:8081'
-//     : 'npm:@blockchain-lab-um/masca';
+const snapId =
+  process.env.USE_LOCAL === 'true'
+    ? 'local:http://localhost:8081'
+    : 'npm:@blockchain-lab-um/masca';
 
-const snapId = 'npm:@blockchain-lab-um/masca';
+//  const snapId = 'npm:@blockchain-lab-um/masca';
 
 const CheckMetaMaskCompatibility = () => {
   const { changeHasMetaMask, changeHasSnaps } = useGeneralStore((state) => ({
