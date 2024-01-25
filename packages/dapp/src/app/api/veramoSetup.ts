70e4d0ac190aed553051dfa8bb061dcbc549d2a2
Urban Vidovič <urbanfoundit@gmail.com>
2024-01-25 08:53:09 +0100

    chore: setup veramo for vc issuing
    Signed-off-by: Urban Vidovič <urbanfoundit@gmail.com>

--- packages/dapp/src/app/api/veramoSetup.ts
+++ packages/dapp/src/app/api/veramoSetup.ts
@@ -1,6 +1,7 @@
 import { getDidKeyResolver as didKeyResolver } from '@blockchain-lab-um/did-provider-key';
 import {
   createAgent,
+  IIdentifier,
   type ICredentialVerifier,
   type IDIDManager,
   type IKeyManager,
@@ -12,15 +13,27 @@ import {
   CredentialPlugin,
   type ICredentialIssuer,
 } from '@veramo/credential-w3c';
+import { AbstractDIDStore, DIDManager } from '@veramo/did-manager';
+import { EthrDIDProvider } from '@veramo/did-provider-ethr';
 import { getDidPkhResolver as didPkhResolver } from '@veramo/did-provider-pkh';
 import { DIDResolverPlugin } from '@veramo/did-resolver';
+import {
+  KeyManager,
+  MemoryKeyStore,
+  MemoryPrivateKeyStore,
+} from '@veramo/key-manager';
+import { KeyManagementSystem } from '@veramo/kms-local';
 import { Resolver } from 'did-resolver';
 import { getResolver as didEnsResolver } from 'ens-did-resolver';
 import { JsonRpcProvider } from 'ethers';
 import { getResolver as didEthrResolver } from 'ethr-did-resolver';
 
 export type Agent = TAgent<
-  IDIDManager & IKeyManager & IResolver & ICredentialVerifier
+  IDIDManager &
+    IKeyManager &
+    IResolver &
+    ICredentialVerifier &
+    ICredentialIssuer
 >;
 
 const networks = [
@@ -50,7 +63,23 @@ export const getAgent = async (): Promise<Agent> => {
     plugins: [
       new CredentialPlugin(),
       new CredentialIssuerEIP712(),
-
+      new DIDManager({
+        // eslint-disable-next-line @typescript-eslint/no-use-before-define
+        store: new MemoryDIDStore(),
+        defaultProvider: 'did:ens',
+        providers: {
+          'did:ethr': new EthrDIDProvider({
+            defaultKms: 'local',
+            networks,
+          }),
+        },
+      }),
+      new KeyManager({
+        store: new MemoryKeyStore(),
+        kms: {
+          local: new KeyManagementSystem(new MemoryPrivateKeyStore()),
+        },
+      }),
       new DIDResolverPlugin({
         resolver: new Resolver({
           ...didEthrResolver({ networks }),
@@ -66,3 +95,35 @@ export const getAgent = async (): Promise<Agent> => {
 
   return agent;
 };
+
+class MemoryDIDStore extends AbstractDIDStore {
+  #store = new Map<string, IIdentifier>();
+
+  async importDID(did: IIdentifier): Promise<boolean> {
+    this.#store.set(did.did, did);
+    return true;
+  }
+
+  async getDID(args: {
+    did: string;
+    alias: string;
+    provider: string;
+  }): Promise<IIdentifier> {
+    const did = this.#store.get(args.did);
+    if (!did) throw Error('DID not found');
+    return did;
+  }
+
+  async deleteDID(args: { did: string }): Promise<boolean> {
+    const did = this.#store.delete(args.did);
+    if (!did) throw Error('DID not found');
+    return true;
+  }
+
+  async listDIDs(args: {
+    alias?: string;
+    provider?: string;
+  }): Promise<IIdentifier[]> {
+    return Array.from(this.#store.values());
+  }
+}
