eb61377945a2aa25c93f190269b570e088ede9a2
Urban Vidovič <urbanfoundit@gmail.com>
2024-01-26 19:50:42 +0100

    fix: error when connecting with unsupported network
    Signed-off-by: Urban Vidovič <urbanfoundit@gmail.com>

--- packages/dapp/src/components/MascaProvider/index.tsx
+++ packages/dapp/src/components/MascaProvider/index.tsx
@@ -2,9 +2,10 @@
 
 import { useEffect } from 'react';
 import { enableMasca, isError } from '@blockchain-lab-um/masca-connector';
-import { useAccount, useChainId } from 'wagmi';
+import { useTranslations } from 'next-intl';
+import { useAccount, useChainId, useSwitchChain } from 'wagmi';
 
-import { useMascaStore } from '@/stores';
+import { useMascaStore, useToastStore } from '@/stores';
 import { useAuthStore } from '@/stores/authStore';
 
 const snapId =
@@ -15,6 +16,8 @@ const snapId =
 const MascaProvider = () => {
   const { address, isConnected } = useAccount();
   const chainId = useChainId();
+  const { switchChainAsync } = useSwitchChain();
+  const t = useTranslations('MascaProvider');
 
   const {
     api,
@@ -103,7 +106,31 @@ const MascaProvider = () => {
 
   useEffect(() => {
     if (!address) return;
-    enableMascaHandler().catch((err) => {
+    enableMascaHandler().catch(async (err) => {
+      // FIXME: this is only a temporary solution
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-call
+      if (err.message.toLowerCase().includes('unsupported network')) {
+        useToastStore.setState({
+          open: true,
+          title: t('unsupported-network'),
+          text: t('unsupported-network-description'),
+          type: 'error',
+          loading: false,
+          link: null,
+        });
+        await switchChainAsync({ chainId: 1 });
+        await enableMascaHandler();
+        return;
+      }
+      setTimeout(() => {
+        useToastStore.setState({
+          open: true,
+          title: t('connection-failed'),
+          type: 'error',
+          loading: false,
+          link: null,
+        });
+      }, 200);
       console.error(err);
     });
   }, [isConnected, address]);
