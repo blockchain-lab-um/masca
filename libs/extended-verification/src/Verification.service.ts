d40c2a688c0b47d1db521a3582674d4ccd546544
martines3000 <domajnko.martin@gmail.com>
2024-02-07 11:16:11 +0100

    feat: add library

---
+++ libs/extended-verification/src/Verification.service.ts
@@ -0,0 +1,5 @@
+export class VerificationService {
+  async verify() {
+    return true;
+  }
+}
---
+++ libs/extended-verification/tests/Verification.service.spec.ts
@@ -0,0 +1,5 @@
+import { describe, it } from 'vitest';
+
+describe('Verification Service', () => {
+  it.todo('should be created');
+});
