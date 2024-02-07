2e81a4f186f9189e27604a4f4fbaabb6245f1151
Tadej <tadej.podrekar@gmail.com>
2024-02-08 00:36:32 +0100

    chore: lint fix

--- packages/dapp/src/utils/format.ts
+++ packages/dapp/src/utils/format.ts
@@ -4,12 +4,12 @@ import { CredentialSubject } from '@veramo/core';
 export const getLastWord = (str: string) => {
   const parts = str.trim().split(',');
   return parts[parts.length - 1];
-}
+};
 
 export const getFirstWord = (str: string) => {
   const parts = str.trim().split(',');
   return parts[0];
-}
+};
 
 export const stringifyCredentialSubject = (
   queryCredentialsRequestResult: QueryCredentialsRequestResult
