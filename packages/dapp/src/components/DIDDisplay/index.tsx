92ebceec60afe810bc68de21fac99a9ca17aa719
Tadej <tadej.podrekar@gmail.com>
2024-02-07 23:54:30 +0100

    fix: inline did copy button

--- packages/dapp/src/components/DIDDisplay/index.tsx
+++ packages/dapp/src/components/DIDDisplay/index.tsx
@@ -8,7 +8,7 @@ export const DIDDisplay = ({ did }: { did: string }) => {
   const t = useTranslations('DIDDisplay');
 
   return (
-    <>
+    <div className="flex flex-row">
       <Tooltip
         content={t('tooltip')}
         className="border-navy-blue-300 bg-navy-blue-100 text-navy-blue-700"
@@ -25,6 +25,6 @@ export const DIDDisplay = ({ did }: { did: string }) => {
       <button className="pl-1" onClick={() => copyToClipboard(did)}>
         <DocumentDuplicateIcon className="animated-transition dark:text-navy-blue-300 ml-1 h-5 w-5 text-gray-700 hover:text-gray-700" />
       </button>
-    </>
+    </div>
   );
 };
