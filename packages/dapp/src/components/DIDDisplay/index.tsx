f83448962bc8cbf0d3b797d283275740c7813032
tadejpodrekar <tadej.podrekar@gmail.com>
2024-02-15 10:46:47 +0100

    fix: minor visual bugs (#562)

--- packages/dapp/src/components/DIDDisplay/index.tsx
+++ packages/dapp/src/components/DIDDisplay/index.tsx
@@ -8,23 +8,28 @@ export const DIDDisplay = ({ did }: { did: string }) => {
   const t = useTranslations('DIDDisplay');
 
   return (
-    <>
-      <Tooltip
-        content={t('tooltip')}
-        className="border-navy-blue-300 bg-navy-blue-100 text-navy-blue-700"
-      >
-        <a
-          href={`https://dev.uniresolver.io/#${did}`}
-          target="_blank"
-          rel="noopener noreferrer"
-          className="text-md animated-transition dark:text-navy-blue-300 cursor-pointer font-normal text-gray-700 underline underline-offset-2"
+    <div>
+      <h2 className="dark:text-navy-blue-200 pr-2 font-bold text-gray-800">
+        DID:
+      </h2>
+      <div className="flex flex-row">
+        <Tooltip
+          content={t('tooltip')}
+          className="border-navy-blue-300 bg-navy-blue-100 text-navy-blue-700"
         >
-          {did.length <= 32 ? did : `${did.slice(0, 16)}...${did.slice(-4)}`}
-        </a>
-      </Tooltip>
-      <button className="pl-1" onClick={() => copyToClipboard(did)}>
-        <DocumentDuplicateIcon className="animated-transition dark:text-navy-blue-300 ml-1 h-5 w-5 text-gray-700 hover:text-gray-700" />
-      </button>
-    </>
+          <a
+            href={`https://dev.uniresolver.io/#${did}`}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-md animated-transition dark:text-navy-blue-300 cursor-pointer font-normal text-gray-700 underline underline-offset-2"
+          >
+            {did.length <= 32 ? did : `${did.slice(0, 16)}...${did.slice(-4)}`}
+          </a>
+        </Tooltip>
+        <button className="pl-1" onClick={() => copyToClipboard(did)}>
+          <DocumentDuplicateIcon className="animated-transition dark:text-navy-blue-300 ml-1 h-5 w-5 text-gray-700 hover:text-gray-700" />
+        </button>
+      </div>
+    </div>
   );
 };
