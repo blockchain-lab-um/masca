35c7ef54faea4c439993577090df7ffadc3df35b
Urban Vidovič <urbanfoundit@gmail.com>
2024-02-19 16:35:40 +0100

    fix: edit metadata on shared credentials page
    Signed-off-by: Urban Vidovič <urbanfoundit@gmail.com>

--- packages/dapp/src/app/[locale]/app/(public)/share-presentation/[id]/page.tsx
+++ packages/dapp/src/app/[locale]/app/(public)/share-presentation/[id]/page.tsx
@@ -163,7 +163,7 @@ export async function generateMetadata({
     );
   }
   return {
-    title: 'Check out my shared credentials',
+    title: 'Shared Credentials',
     description: 'Page for displaying shared presentations',
     openGraph: {
       type: 'article',
