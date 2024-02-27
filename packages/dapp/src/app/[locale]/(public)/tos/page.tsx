84b67751a30a79a70a08cf09c5a7e6ed1111b448
Urban Vidovič <urbanfoundit@gmail.com>
2024-02-27 17:40:21 +0100

    feat: add tos and privacy policy pages
    Signed-off-by: Urban Vidovič <urbanfoundit@gmail.com>

---
+++ packages/dapp/src/app/[locale]/(public)/tos/page.tsx
@@ -0,0 +1,44 @@
+import DOMPurify from 'dompurify';
+import { JSDOM } from 'jsdom';
+import { marked } from 'marked';
+
+const getTos = async () => {
+  const renderer = {
+    heading(text: any, level: any) {
+      switch (level) {
+        case 1:
+          return `
+            <h${level} class="text-2xl pt-2">
+              ${text}
+            </h${level}>`;
+        case 2:
+          return `
+            <h${level} class="text-xl pt-2">
+              ${text}
+            </h${level}>`;
+        default:
+          return `<h${level} class="text-lg"> ${text} </h${level}>`;
+      }
+    },
+  };
+
+  marked.use({ renderer });
+  const markdown = await (
+    await fetch(
+      'https://raw.githubusercontent.com/lutralabs/documents/main/ToS.md',
+      { cache: 'no-store' }
+    )
+  ).text();
+  const { window } = new JSDOM('');
+  const purify = DOMPurify(window);
+  const html = purify.sanitize(await marked.parse(markdown));
+  return html;
+};
+
+export default async function Page() {
+  const tos = await getTos();
+  <div
+    className="h-full w-full max-w-2xl flex-1 flex-col p-16"
+    dangerouslySetInnerHTML={{ __html: tos }}
+  />;
+}
