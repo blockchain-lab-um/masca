3374733eb084a05e907b345888a6382e69318df4
tadejpodrekar <tadej.podrekar@gmail.com>
2024-03-14 10:08:11 +0100

    chore: remove unused route

--- packages/dapp/src/utils/supabase/publicClient.ts
+++ packages/dapp/src/utils/supabase/publicClient.ts
@@ -5,7 +5,7 @@ import {
 import { Database } from './database.types';
 
 const SUPABASE_URL: string = process.env.NEXT_PUBLIC_SUPABASE_URL!;
-const SUPABASE_ANON_KEY: string = process.env.SUPABASE_SECRET_KEY!;
+const SUPABASE_SECRET_KEY: string = process.env.SUPABASE_SECRET_KEY!;
 
 let cachedSupabaseClient: SupabaseClient<Database> | null = null;
 
@@ -16,7 +16,7 @@ export const createPublicClient = () => {
 
   const supabase = createSupbaseClient<Database>(
     SUPABASE_URL,
-    SUPABASE_ANON_KEY
+    SUPABASE_SECRET_KEY
   );
 
   cachedSupabaseClient = supabase;
