75ab1d65c36dd212e77b84edcdfb94bda6373fc7
martines3000 <domajnko.martin@gmail.com>
2024-03-15 16:49:58 +0100

    chore: move remove use client from page

--- packages/dapp/src/app/[locale]/app/(public)/campaigns/page.tsx
+++ packages/dapp/src/app/[locale]/app/(public)/campaigns/page.tsx
@@ -1,17 +1,9 @@
-'use client';
-
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-
 import { CampaignsDisplay } from '@/components/CampaignsDisplay';
-
-const queryClient = new QueryClient();
 
 export default function Page() {
   return (
     <div className="flex h-full flex-1 justify-center">
-      <QueryClientProvider client={queryClient}>
-        <CampaignsDisplay />
-      </QueryClientProvider>
+      <CampaignsDisplay />
     </div>
   );
 }
--- packages/dapp/src/components/CampaignsDisplay/index.tsx
+++ packages/dapp/src/components/CampaignsDisplay/index.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import React from 'react';
 import { isError } from '@blockchain-lab-um/masca-connector';
 import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
