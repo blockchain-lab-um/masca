ae11eaed0dd823601dd660dba67ae77ff6ee238d
martines3000 <domajnko.martin@gmail.com>
2024-03-15 14:03:06 +0100

    fix: improve supabase client

--- packages/dapp/src/utils/supabase/supabaseClient.ts
+++ packages/dapp/src/utils/supabase/supabaseClient.ts
@@ -1,16 +1,17 @@
 import { createClient, SupabaseClient } from '@supabase/supabase-js';
 import { Database } from './database.types';
+import { useMemo } from 'react';
 
 let client: SupabaseClient<Database> | null = null;
 let token: string | null = null;
 
-export const supabaseClient = (accessToken?: string | null) => {
+const createSupabaseClient = (accessToken?: string | null) => {
   if (token !== accessToken) {
     token = accessToken || null;
     client = null;
   }
 
-  if (!client) {
+  if (client === null) {
     client = createClient<Database>(
       process.env.NEXT_PUBLIC_SUPABASE_URL!,
       process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
@@ -23,4 +24,8 @@ export const supabaseClient = (accessToken?: string | null) => {
   }
 
   return client;
+};
+
+export const supabaseClient = (accessToken?: string | null) => {
+  return useMemo(() => createSupabaseClient(accessToken), [accessToken]);
 };
