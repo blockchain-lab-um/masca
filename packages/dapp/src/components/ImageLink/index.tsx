8bd058c299aa3be5bf5902db7cac0a16796bae47
Urban Vidovič <urbanfoundit@gmail.com>
2024-03-20 14:33:43 +0100

    fix: fallback to public ipfs
    Signed-off-by: Urban Vidovič <urbanfoundit@gmail.com>

--- packages/dapp/src/components/ImageLink/index.tsx
+++ packages/dapp/src/components/ImageLink/index.tsx
@@ -10,9 +10,9 @@ export const ImageLink = ({ value }: { value: string }) => {
       </h2>
       <div className="flex flex-row">
         <a
-          href={`${process.env.NEXT_PUBLIC_IPFS_GATEWAY}/ipfs/${value
-            .toString()
-            .slice('ipfs://'.length)}`}
+          href={`${
+            process.env.NEXT_PUBLIC_IPFS_GATEWAY || 'https://ipfs.io'
+          }/ipfs/${value.toString().slice('ipfs://'.length)}`}
           target="_blank"
           rel="noopener noreferrer"
           className="text-md animated-transition dark:text-navy-blue-300 cursor-pointer font-normal text-gray-700 underline underline-offset-2"
