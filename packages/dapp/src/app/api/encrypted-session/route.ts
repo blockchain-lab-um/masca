75b587343482e14b46026d197c661bed67514cde
martines3000 <domajnko.martin@gmail.com>
2024-03-25 19:48:49 +0100

    fix: resolve issues with siwe

--- packages/dapp/src/app/api/encrypted-session/route.ts
+++ packages/dapp/src/app/api/encrypted-session/route.ts
@@ -1,118 +1,118 @@
-import { type NextRequest, NextResponse } from "next/server";
-import { createClient } from "@supabase/supabase-js";
-import jwt from "jsonwebtoken";
+import { NextResponse, type NextRequest } from 'next/server';
+import { createClient } from '@supabase/supabase-js';
+import jwt from 'jsonwebtoken';
 
-import type { Database } from "@/utils/supabase/database.types";
+import type { Database } from '@/utils/supabase/database.types';
 
 const CORS_HEADERS = {
-	"Access-Control-Allow-Origin": "*",
-	"Access-Control-Allow-Methods": "GET OPTIONS",
-	"Access-Control-Allow-Headers": "Content-Type",
+  'Access-Control-Allow-Origin': '*',
+  'Access-Control-Allow-Methods': 'GET OPTIONS',
+  'Access-Control-Allow-Headers': 'Content-Type',
 };
 
 export async function GET(request: NextRequest) {
-	try {
-		const token = request.headers.get("Authorization")?.replace("Bearer ", "");
+  try {
+    const token = request.headers.get('Authorization')?.replace('Bearer ', '');
 
-		if (!token) {
-			return new NextResponse("Unauthorized", {
-				status: 401,
-				headers: {
-					...CORS_HEADERS,
-				},
-			});
-		}
+    if (!token) {
+      return new NextResponse('Unauthorized', {
+        status: 401,
+        headers: {
+          ...CORS_HEADERS,
+        },
+      });
+    }
 
-		const user = jwt.verify(token, process.env.SUPABASE_JWT_SECRET!) as {
-			sub: string;
-			address: string;
-			aud: string;
-			role: string;
-			iat: number;
-			exp: number;
-		};
+    const user = jwt.verify(token, process.env.SUPABASE_JWT_SECRET!) as {
+      sub: string;
+      address: string;
+      aud: string;
+      role: string;
+      iat: number;
+      exp: number;
+    };
 
-		const supabase = createClient<Database>(
-			process.env.NEXT_PUBLIC_SUPABASE_URL!,
-			process.env.SUPABASE_SECRET_KEY!,
-		);
+    const supabase = createClient<Database>(
+      process.env.NEXT_PUBLIC_SUPABASE_URL!,
+      process.env.SUPABASE_SECRET_KEY!
+    );
 
-		const { data: selectData, error: selectError } = await supabase
-			.from("sessions")
-			.select("id")
-			.eq("user_id", user.sub);
+    const { data: selectData, error: selectError } = await supabase
+      .from('sessions')
+      .select('id')
+      .eq('user_id', user.sub);
 
-		if (selectError) {
-			return new NextResponse("Internal Server Error", {
-				status: 500,
-				headers: {
-					...CORS_HEADERS,
-				},
-			});
-		}
+    if (selectError) {
+      return new NextResponse('Internal Server Error', {
+        status: 500,
+        headers: {
+          ...CORS_HEADERS,
+        },
+      });
+    }
 
-		// If session is found delete it
-		if (selectData.length !== 0) {
-			const { error: deleteError } = await supabase
-				.from("sessions")
-				.delete()
-				.eq("user_id", user.sub);
+    // If session is found delete it
+    if (selectData.length !== 0) {
+      const { error: deleteError } = await supabase
+        .from('sessions')
+        .delete()
+        .eq('user_id', user.sub);
 
-			if (deleteError) {
-				return new NextResponse("Internal Server Error", {
-					status: 500,
-					headers: {
-						...CORS_HEADERS,
-					},
-				});
-			}
-		}
+      if (deleteError) {
+        return new NextResponse('Internal Server Error', {
+          status: 500,
+          headers: {
+            ...CORS_HEADERS,
+          },
+        });
+      }
+    }
 
-		// Create a new session
-		const { data: insertData, error: insertError } = await supabase
-			.from("sessions")
-			.insert({
-				user_id: user.sub,
-			})
-			.select()
-			.limit(1)
-			.single();
+    // Create a new session
+    const { data: insertData, error: insertError } = await supabase
+      .from('sessions')
+      .insert({
+        user_id: user.sub,
+      })
+      .select()
+      .limit(1)
+      .single();
 
-		if (insertError || !insertData) {
-			return new NextResponse("Internal Server Error", {
-				status: 500,
-				headers: {
-					...CORS_HEADERS,
-				},
-			});
-		}
+    if (insertError || !insertData) {
+      return new NextResponse('Internal Server Error', {
+        status: 500,
+        headers: {
+          ...CORS_HEADERS,
+        },
+      });
+    }
 
-		return NextResponse.json(
-			{
-				sessionId: insertData.id,
-			},
-			{
-				status: 201,
-				headers: {
-					...CORS_HEADERS,
-				},
-			},
-		);
-	} catch (error) {
-		return new NextResponse("Internal Server Error", {
-			status: 500,
-			headers: {
-				...CORS_HEADERS,
-			},
-		});
-	}
+    return NextResponse.json(
+      {
+        sessionId: insertData.id,
+      },
+      {
+        status: 201,
+        headers: {
+          ...CORS_HEADERS,
+        },
+      }
+    );
+  } catch (error) {
+    return new NextResponse('Internal Server Error', {
+      status: 500,
+      headers: {
+        ...CORS_HEADERS,
+      },
+    });
+  }
 }
 
 export async function OPTIONS() {
-	return new NextResponse(null, {
-		status: 200,
-		headers: {
-			...CORS_HEADERS,
-		},
-	});
+  return new NextResponse(null, {
+    status: 200,
+    headers: {
+      ...CORS_HEADERS,
+    },
+  });
 }
--- packages/dapp/src/components/EncryptedSessionDisplay/ConnectDeviceView.tsx
+++ packages/dapp/src/components/EncryptedSessionDisplay/ConnectDeviceView.tsx
@@ -1,185 +1,185 @@
-import React, { useEffect, useState } from "react";
-import { createClient as createSupbaseClient } from "@supabase/supabase-js";
-import { useTranslations } from "next-intl";
-import { useAccount } from "wagmi";
+import React, { useEffect, useState } from 'react';
+import { createClient as createSupbaseClient } from '@supabase/supabase-js';
+import { useTranslations } from 'next-intl';
+import { useAccount } from 'wagmi';
 
-import Button from "@/components/Button";
-import CreateConnectionModal from "@/components/ConnectionModal/CreateConnectionModal";
-import ScanQRCodeModal from "@/components/ScanQRCodeModal/ScanQRCodeModal";
-import type { Database } from "@/utils/supabase/database.types";
-import { useEncryptedSessionStore, useToastStore } from "@/stores";
-import { useAuthStore } from "@/stores/authStore";
+import Button from '@/components/Button';
+import CreateConnectionModal from '@/components/ConnectionModal/CreateConnectionModal';
+import ScanQRCodeModal from '@/components/ScanQRCodeModal/ScanQRCodeModal';
+import type { Database } from '@/utils/supabase/database.types';
+import { useEncryptedSessionStore, useToastStore } from '@/stores';
+import { useAuthStore } from '@/stores/authStore';
 
 export const ConnectDeviceView = () => {
-	const t = useTranslations("ConnectDeviceView");
+  const t = useTranslations('ConnectDeviceView');
 
-	// Local state
-	const [isModalOpen, setIsModalOpen] = useState(false);
-	const [isConnectionModalOpen, setIsConnectionModalOpen] = useState(false);
+  // Local state
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [isConnectionModalOpen, setIsConnectionModalOpen] = useState(false);
 
-	// Global state
-	const { isSignedIn, changeIsSignInModalOpen } = useAuthStore((state) => ({
-		isSignedIn: state.isSignedIn,
-		changeIsSignInModalOpen: state.changeIsSignInModalOpen,
-	}));
+  // Global state
+  const { isSignedIn, changeIsSignInModalOpen } = useAuthStore((state) => ({
+    isSignedIn: state.isSignedIn,
+    changeIsSignInModalOpen: state.changeIsSignInModalOpen,
+  }));
 
-	const { isConnected } = useAccount();
-	const {
-		connected,
-		deviceType,
-		hasCamera,
-		changeSession,
-		changeConnected,
-		changeSessionId,
-	} = useEncryptedSessionStore((state) => ({
-		session: state.session,
-		connected: state.connected,
-		deviceType: state.deviceType,
-		hasCamera: state.hasCamera,
-		changeSession: state.changeSession,
-		changeConnected: state.changeConnected,
-		changeSessionId: state.changeSessionId,
-	}));
+  const { isConnected } = useAccount();
+  const {
+    connected,
+    deviceType,
+    hasCamera,
+    changeSession,
+    changeConnected,
+    changeSessionId,
+  } = useEncryptedSessionStore((state) => ({
+    session: state.session,
+    connected: state.connected,
+    deviceType: state.deviceType,
+    hasCamera: state.hasCamera,
+    changeSession: state.changeSession,
+    changeConnected: state.changeConnected,
+    changeSessionId: state.changeSessionId,
+  }));
 
-	useEffect(() => {
-		// Close connect QR modal if connection is established
-		if (connected && isModalOpen) {
-			setIsModalOpen(false);
-		}
-	}, [connected]);
+  useEffect(() => {
+    // Close connect QR modal if connection is established
+    if (connected && isModalOpen) {
+      setIsModalOpen(false);
+    }
+  }, [connected]);
 
-	const onScanSuccessConnectionQRCode = async (decodedText: string, _: any) => {
-		if (isConnectionModalOpen) {
-			setIsConnectionModalOpen(false);
-		}
-		// Close if already connected
-		if (connected) return;
+  const onScanSuccessConnectionQRCode = async (decodedText: string, _: any) => {
+    if (isConnectionModalOpen) {
+      setIsConnectionModalOpen(false);
+    }
+    // Close if already connected
+    if (connected) return;
 
-		try {
-			const data = JSON.parse(decodedText);
+    try {
+      const data = JSON.parse(decodedText);
 
-			if (!data.sessionId || !data.keyData || !data.exp) throw new Error();
+      if (!data.sessionId || !data.keyData || !data.exp) throw new Error();
 
-			const decryptionKey = await crypto.subtle.importKey(
-				"jwk",
-				data.keyData,
-				{ name: "AES-GCM", length: 256 },
-				true,
-				["encrypt", "decrypt"],
-			);
+      const decryptionKey = await crypto.subtle.importKey(
+        'jwk',
+        data.keyData,
+        { name: 'AES-GCM', length: 256 },
+        true,
+        ['encrypt', 'decrypt']
+      );
 
-			// Send data
-			const client = createSupbaseClient<Database>(
-				process.env.NEXT_PUBLIC_SUPABASE_URL!,
-				process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
-			);
+      // Send data
+      const client = createSupbaseClient<Database>(
+        process.env.NEXT_PUBLIC_SUPABASE_URL!,
+        process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+      );
 
-			const { error } = await client
-				.from("sessions")
-				.update({
-					connected: true,
-				})
-				.eq("id", data.sessionId);
+      const { error } = await client
+        .from('sessions')
+        .update({
+          connected: true,
+        })
+        .eq('id', data.sessionId);
 
-			if (error) throw new Error("Failed to send data");
+      if (error) throw new Error('Failed to send data');
 
-			changeSession({
-				key: decryptionKey,
-				exp: data.exp,
-			});
-			changeConnected(true);
-			changeSessionId(data.sessionId);
+      changeSession({
+        key: decryptionKey,
+        exp: data.exp,
+      });
+      changeConnected(true);
+      changeSessionId(data.sessionId);
 
-			setTimeout(() => {
-				useToastStore.setState({
-					open: true,
-					title: t("success"),
-					type: "success",
-					loading: false,
-					link: null,
-				});
-			}, 200);
-		} catch (e) {
-			console.log(e);
-			setTimeout(() => {
-				useToastStore.setState({
-					open: true,
-					title: t("invalid"),
-					type: "error",
-					loading: false,
-					link: null,
-				});
-			}, 200);
-		}
-	};
+      setTimeout(() => {
+        useToastStore.setState({
+          open: true,
+          title: t('success'),
+          type: 'success',
+          loading: false,
+          link: null,
+        });
+      }, 200);
+    } catch (e) {
+      console.log(e);
+      setTimeout(() => {
+        useToastStore.setState({
+          open: true,
+          title: t('invalid'),
+          type: 'error',
+          loading: false,
+          link: null,
+        });
+      }, 200);
+    }
+  };
 
-	return (
-		<div className="">
-			{deviceType === "primary" && !hasCamera && (
-				<>
-					{isConnected && (
-						<>
-							<div className="dark:bg-navy-blue-700 rounded-xl bg-gray-100 p-4">
-								<div>{t("start-primary")}</div>
-								<div className="mt-2">
-									{`Press the 'Create Connection' button below and Scan the QR
+  return (
+    <div className="">
+      {deviceType === 'primary' && !hasCamera && (
+        <>
+          {isConnected && (
+            <>
+              <div className="dark:bg-navy-blue-700 rounded-xl bg-gray-100 p-4">
+                <div>{t('start-primary')}</div>
+                <div className="mt-2">
+                  {`Press the 'Create Connection' button below and Scan the QR
                   code on your secondary device`}
-								</div>
-							</div>
-							<div className="mt-8 flex justify-center">
-								<Button
-									variant="primary"
-									onClick={() => {
-										if (!isSignedIn) {
-											changeIsSignInModalOpen(true);
-											return;
-										}
-										setIsModalOpen(true);
-									}}
-								>
-									{t("create")}
-								</Button>
-							</div>
-						</>
-					)}
-					{!isConnected && <div>{t("connect")}</div>}
-				</>
-			)}
-			{hasCamera && (
-				<>
-					{deviceType === "secondary" && (
-						<>
-							<div className="dark:bg-navy-blue-700 rounded-xl bg-gray-100 p-4">
-								<div>{t("start-secondary")}</div>
-							</div>
-							<div className="mt-8 flex justify-center">
-								<Button
-									variant="primary"
-									onClick={() => {
-										// Reset session if already set
-										changeSessionId(null);
-										changeSession({
-											key: null,
-											exp: null,
-										});
-										changeConnected(false);
-										setIsConnectionModalOpen(true);
-									}}
-								>
-									{t("scan")}
-								</Button>
-							</div>
-						</>
-					)}
-				</>
-			)}
-			<CreateConnectionModal isOpen={isModalOpen} setOpen={setIsModalOpen} />
-			<ScanQRCodeModal
-				onScanSuccess={onScanSuccessConnectionQRCode}
-				title={"Scan Connection"}
-				isOpen={isConnectionModalOpen}
-				setOpen={setIsConnectionModalOpen}
-			/>
-		</div>
-	);
+                </div>
+              </div>
+              <div className="mt-8 flex justify-center">
+                <Button
+                  variant="primary"
+                  onClick={() => {
+                    if (!isSignedIn) {
+                      changeIsSignInModalOpen(true);
+                      return;
+                    }
+                    setIsModalOpen(true);
+                  }}
+                >
+                  {t('create')}
+                </Button>
+              </div>
+            </>
+          )}
+          {!isConnected && <div>{t('connect')}</div>}
+        </>
+      )}
+      {hasCamera && (
+        <>
+          {deviceType === 'secondary' && (
+            <>
+              <div className="dark:bg-navy-blue-700 rounded-xl bg-gray-100 p-4">
+                <div>{t('start-secondary')}</div>
+              </div>
+              <div className="mt-8 flex justify-center">
+                <Button
+                  variant="primary"
+                  onClick={() => {
+                    // Reset session if already set
+                    changeSessionId(null);
+                    changeSession({
+                      key: null,
+                      exp: null,
+                    });
+                    changeConnected(false);
+                    setIsConnectionModalOpen(true);
+                  }}
+                >
+                  {t('scan')}
+                </Button>
+              </div>
+            </>
+          )}
+        </>
+      )}
+      <CreateConnectionModal isOpen={isModalOpen} setOpen={setIsModalOpen} />
+      <ScanQRCodeModal
+        onScanSuccess={onScanSuccessConnectionQRCode}
+        title={'Scan Connection'}
+        isOpen={isConnectionModalOpen}
+        setOpen={setIsConnectionModalOpen}
+      />
+    </div>
+  );
 };
