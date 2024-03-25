44fe8b4d074a00594a2d8d5f103638c89d647ba5
Urban Vidovič <urbanfoundit@gmail.com>
2024-03-25 09:38:20 +0100

    feat: add description and title to campaigns page
    Signed-off-by: Urban Vidovič <urbanfoundit@gmail.com>

--- packages/dapp/src/components/CampaignsDisplay/index.tsx
+++ packages/dapp/src/components/CampaignsDisplay/index.tsx
@@ -26,6 +26,12 @@ export const CampaignsDisplay = () => {
 
   return (
     <div className="flex w-full max-w-4xl flex-col gap-y-4">
+      <div className="flex flex-col gap-y-4">
+        <h5 className="font-ubuntu dark:text-navy-blue-200 mt-8 text-2xl font-medium leading-6 text-gray-700">
+          {t('title')}
+        </h5>
+        <p className="text-justify">{t('description')}</p>
+      </div>
       {campaigns.map((campaign) => (
         <CampaignDisplay key={campaign.id} campaign={campaign} />
       ))}
