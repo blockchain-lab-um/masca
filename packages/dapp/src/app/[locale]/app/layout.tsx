4c6a248cb6c300a17d427f40da6cf57b897677c3
Urban Vidovič <urbanfoundit@gmail.com>
2024-04-23 21:41:50 +0200

    feat(dapp): add banner
    Signed-off-by: Urban Vidovič <urbanfoundit@gmail.com>

--- packages/dapp/src/app/[locale]/app/layout.tsx
+++ packages/dapp/src/app/[locale]/app/layout.tsx
@@ -6,6 +6,7 @@ import { SignInModal } from '@/components/SignInModal';
 import ToastWrapper from '@/components/ToastWrapper';
 import { Providers } from '@/components/Providers';
 import { ScrollShadow } from '@nextui-org/react';
+import Banner from '@/components/Banner';
 
 export default async function AppLayout({
   children,
@@ -14,17 +15,22 @@ export default async function AppLayout({
 }) {
   return (
     <Providers>
-      <div className="flex flex-col h-screen px-4 pb-6 sm:px-12">
-        <div className="p-6 m-0 sm:px-2 max-sm:py-2">
-          <AppNavbar />
+      <div className="flex flex-col h-screen pb-6">
+        <div>
+          <Banner />
         </div>
-        <ScrollShadow
-          className="flex h-full w-full flex-col py-6 px-2 main-bg max-sm:mb-12"
-          hideScrollBar
-          size={8}
-        >
-          {children}
-        </ScrollShadow>
+        <div className="flex flex-col h-full px-4 pb-6 sm:px-12">
+          <div className="p-6 m-0 sm:px-2 max-sm:py-2">
+            <AppNavbar />
+          </div>
+          <ScrollShadow
+            className="flex h-full w-full flex-col py-6 px-2 main-bg max-sm:mb-12"
+            hideScrollBar
+            size={8}
+          >
+            {children}
+          </ScrollShadow>
+        </div>
       </div>
       <AppBottomBar />
       <SignInModal />
