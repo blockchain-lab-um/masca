c8296242be92d0a76e3d762f952d2d549a570ab0
Urban <urbanfoundit@gmail.com>
2024-04-23 21:50:03 +0200

    feat(dapp): banner (#634)
    Signed-off-by: Urban Vidovič <urbanfoundit@gmail.com>

---
+++ .changeset/lemon-snakes-taste.md
@@ -0,0 +1,5 @@
+---
+"@blockchain-lab-um/dapp": patch
+---
+
+Adds banner at the top of dapp.
---
+++ packages/dapp/src/components/Banner/index.tsx
@@ -0,0 +1,21 @@
+import { Link } from '@nextui-org/react';
+
+export default function Banner() {
+  return (
+    <div className="bg-pink-200 text-center dark:bg-navy-blue-800">
+      <div className="container mx-auto px-4">
+        <p>
+          Support Masca on{' '}
+          <Link
+            className="animate-pulse"
+            target="_blank"
+            href="https://explorer.gitcoin.co/#/round/42161/25/162"
+          >
+            Gitcoin Grants
+          </Link>
+          !
+        </p>
+      </div>
+    </div>
+  );
+}
