07d58712c6650333415e1fddc89eb5691361f88d
martines3000 <domajnko.martin@gmail.com>
2024-10-17 17:14:49 +0200

    chore: update packages

--- packages/snap/src/UI.service.tsx
+++ packages/snap/src/UI.service.tsx
@@ -26,6 +26,7 @@ import {
   Italic,
   Link,
   type JSXElement,
+  type SnapComponent,
 } from '@metamask/snaps-sdk/jsx';
 import type { W3CVerifiableCredential } from '@veramo/core';
 
@@ -38,6 +39,58 @@ const permissionExtraText: Record<string, string> = {
     'You will no longer see a popup on this dApp when credentials are requested!',
 };
 
+export const InteractiveForm: SnapComponent = () => {
+  return (
+    <Box>
+      <Heading>Interactive UI Example Snap</Heading>
+      <Form name="example-form">
+        <Field label="Example Input">
+          <Input name="example-input" placeholder="Enter something..." />
+        </Field>
+        <Field label="Example Dropdown">
+          <Dropdown name="example-dropdown">
+            <Option value="option1">Option 1</Option>
+            <Option value="option2">Option 2</Option>
+            <Option value="option3">Option 3</Option>
+          </Dropdown>
+        </Field>
+        <Field label="Example RadioGroup">
+          <RadioGroup name="example-radiogroup">
+            <Radio value="option1">Option 1</Radio>
+            <Radio value="option2">Option 2</Radio>
+            <Radio value="option3">Option 3</Radio>
+          </RadioGroup>
+        </Field>
+        <Field label="Example Checkbox">
+          <Checkbox name="example-checkbox" label="Checkbox" />
+        </Field>
+        <Field label="Example Selector">
+          <Selector
+            name="example-selector"
+            title="Choose an option"
+            value="option1"
+          >
+            <SelectorOption value="option1">
+              <Card title="Option 1" value="option1" />
+            </SelectorOption>
+            <SelectorOption value="option2">
+              <Card title="Option 2" value="option2" />
+            </SelectorOption>
+            <SelectorOption value="option3">
+              <Card title="Option 3" value="option3" />
+            </SelectorOption>
+          </Selector>
+        </Field>
+        <Box center>
+          <Button type="submit" name="submit">
+            Submit
+          </Button>
+        </Box>
+      </Form>
+    </Box>
+  );
+};
+
 class UIService {
   static originHostname: string;
 
@@ -46,7 +99,7 @@ class UIService {
   static async init(origin: string) {
     UIService.originHostname = new URL(origin).hostname; // hostname
     UIService.originWrapper = [
-      <Box key={'originWrapperBox'}>
+      <Box key="originWrapperBox">
         <Text>
           Origin: <Bold>{UIService.originHostname}</Bold>
         </Text>
