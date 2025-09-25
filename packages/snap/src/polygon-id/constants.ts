fc45d79fa8fde342650b70f26717d987d80affbd
pegi4 <vito.senic@gmail.com>
2025-09-25 13:26:54 +0200

    fix: polygon-amoy-rpc-url change

--- packages/snap/src/polygon-id/constants.ts
+++ packages/snap/src/polygon-id/constants.ts
@@ -8,8 +8,7 @@ import {
 export const RHS_URL = 'https://rhs-staging.polygonid.me';
 
 export const POLYGON_MAINNET_RPC_URL = 'https://polygon-rpc.com';
-export const POLYGON_AMOY_RPC_URL =
-  'https://polygon-amoy.blockpi.network/v1/rpc/public';
+export const POLYGON_AMOY_RPC_URL = 'https://rpc-amoy.polygon.technology/';
 
 export const CONTRACT_POLYGON_MAINNET =
   '0x624ce98D2d27b20b8f8d521723Df8fC4db71D79D';
